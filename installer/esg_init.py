889d9b543750a8db2d7643e2497ee8235e13d1a2
William Hill <hill119@llnl.gov>
2016-11-02 16:13:10 -0700

    added esg_init.py

---
+++ installer/esg_init.py
@@ -0,0 +1,22 @@
+'''
+	Public
+
+'''
+#--------------
+#User Defined / Settable (public)
+#--------------
+# _t=${0%.*}
+# install_prefix=${install_prefix:-${ESGF_INSTALL_PREFIX:-"/usr/local"}}
+install_prefix = "/usr/local"
+print install_prefix
+# esg_root_dir=${esg_root_dir:-${ESGF_HOME:-"/esg"}}
+# esg_config_dir=${esg_root_dir}/config
+# esg_config_type_file=${esg_config_dir}/config_type
+# esgf_secret_file=${esg_config_dir}/.esgf_pass
+# pg_secret_file=${esg_config_dir}/.esg_pg_pass
+# pub_secret_file=${esg_config_dir}/.esg_pg_publisher_pass
+# ks_secret_file=${esg_config_dir}/.esg_keystore_pass
+# install_manifest=${install_manifest:-"${esg_root_dir}/esgf-install-manifest"}
+# install_logfile=${install_manifest}
+# logfile=${logfile:-"/tmp/${_t##*/}.out"}
+#--------------

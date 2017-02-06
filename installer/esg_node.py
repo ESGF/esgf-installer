61b872afba570232d1dff063b02f41263bbc96d3
William Hill <hill119@llnl.gov>
2017-02-05 21:19:06 -0800

    added the test_esgcet() function

--- installer/esg_node.py
+++ installer/esg_node.py
@@ -51,6 +51,9 @@ if config.config_dictionary["esg_root_id"]:
 else:
     esg_root_id = esg_functions.get_property("esg_root_id")
 
+node_short_name = None
+if not config.config_dictionary["node_short_name"]:
+    node_short_name = esg_functions.get_property("node_short_name")
 # write_java_env() {
 #     ((show_summary_latch++))
 #     echo "export JAVA_HOME=${java_install_dir}" >> ${envfile}
@@ -265,9 +268,6 @@ def setup_esgcet(upgrade_mode = None):
         if org_id_input:
             esg_root_id = org_id_input
 
-        node_short_name = None
-        if not config.config_dictionary["node_short_name"]:
-            node_short_name = esg_functions.get_property("node_short_name")
 
         print "%s/bin/esgsetup --config $( ((%s == 1 )) && echo '--minimal-setup' ) --rootid %s" % (config.config_dictionary["cdat_home"], recommended, esg_root_id)
         os.mkdir(config.config_dictionary["publisher_home"])
@@ -382,7 +382,6 @@ def setup_esgcet(upgrade_mode = None):
 
     esg_functions.checked_done(0)
 
-    pass
 
 def write_esgcet_env():
     # print
@@ -400,6 +399,67 @@ def write_esgcet_install_log():
     esg_functions.write_as_property("publisher_home", config.config_dictionary["publisher_home"])
     esg_functions.write_as_property("monitor.esg.ini", os.path.join(config.config_dictionary["publisher_home"], config.config_dictionary["publisher_config"]))
     return 0
+
+def test_esgcet():
+	print '''
+    	"----------------------------"
+    	"ESGCET Test... "
+    	"----------------------------"
+    '''
+	starting_directory = os.getcwd()
+	os.chdir(config.config_dictionary["workdir"])
+
+	start_postgress()
+
+	esgcet_testdir=os.path.join(config.config_dictionary["thredds_root_dir"], "test")
+	try:
+		os.mkdir(esgcet_testdir)
+	except:
+		esg_functions.checked_done(1)
+
+	os.chown(esgcet_testdir, config.config_dictionary["installer_uid"], config.config_dictionary["installer_gid"])
+
+	try:
+		os.mkdir(config.config_dictionary["thredds_replica_dir"])
+	except:
+		esg_functions.checked_done(1)
+
+	os.chown(config.config_dictionary["thredds_replica_dir"], config.config_dictionary["installer_uid"], config.config_dictionary["installer_gid"])
+	print "esgcet test directory: [%s]" % esgcet_testdir
+
+	fetch_file="sftlf.nc"
+	if esg_functions.checked_get(os.path.join(esgcet_testdir,fetch_file), config.config_dictionary["esg_dist_url_root"]+"/externals/"+fetch_file) > 0:
+		print " ERROR: Problem pulling down %s from esg distribution" % (fetch_file)
+		os.chdir(starting_directory)
+		esg_functions.checked_done(1)
+
+	#Run test...
+	print "%s/bin/esginitialize -c " % (config.config_dictionary["cdat_home"])
+	esginitialize_output = subprocess.call("%s/bin/esginitialize -c" % (config.config_dictionary["cdat_home"]))
+	print '''
+		%s/bin/esgscan_directory --dataset pcmdi.%s.%s.
+		test.mytest --project test %s > mytest.txt
+		''' % (config.config_dictionary["cdat_home"], esg_root_id, node_short_name, esgcet_testdir)
+	esgscan_directory_output = subprocess.call('''
+		%s/bin/esgscan_directory --dataset pcmdi.%s.%s.
+		test.mytest --project test %s > mytest.txt
+		''' % (config.config_dictionary["cdat_home"], esg_root_id, node_short_name, esgcet_testdir))
+	if esgscan_directory_output !=0:
+		print " ERROR: ESG directory scan failed"
+		os.chdir(starting_directory)
+		esg_functions.checked_done(1) 
+
+	print "$cdat_home/bin/esgpublish --service fileservice --map mytest.txt --project test --model test" % (config.config_dictionary["cdat_home"])
+	esgpublish_output = subprocess.call("$cdat_home/bin/esgpublish --service fileservice --map mytest.txt --project test --model test" % (config.config_dictionary["cdat_home"]))
+	if esgpublish_output != 0:
+		print " ERROR: ESG publish failed"
+		os.chdir(starting_directory)
+		esg_functions.checked_done(1)
+
+	os.chdir(starting_directory)
+	esg_functions.checked_done(0) 
+
+	pass
 
 #returns 1 if it is already running (if check_postgress_process returns 0 - true)
 def start_postgress():

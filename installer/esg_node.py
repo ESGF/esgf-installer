31c91bbf2bdfe5d666a0515e17b30e7ebb1a1dfc
Eliakin del Rosario <eking1566@gmail.com>
2017-06-08 09:56:34 -0700

    Removed comments

--- installer/esg_node.py
+++ installer/esg_node.py
@@ -28,8 +28,6 @@ logger.info("keystore_alias: %s", config.config_dictionary["keystore_alias"])
 os.environ['LANG'] = "POSIX"
 os.umask(022)
 
-# I traced the Expand.colonMinus function from esg_bash2py.py module
-# and only got a overall idea of what it does. Does this create and assign values variables? 
 DEBUG = esg_bash2py.Expand.colonMinus("DEBUG", False)
 VERBOSE = esg_bash2py.Expand.colonMinus("VERBOSE", "0")
 # INSTALL_BIT=1
@@ -48,15 +46,11 @@ VERBOSE = esg_bash2py.Expand.colonMinus("VERBOSE", "0")
 
 bit_boolean_dictionary = {"INSTALL_BIT": False , "TEST_BIT": False, "DATA_BIT":False, "INDEX_BIT":False, "IDP_BIT":False, "COMPUTE_BIT":False, "WRITE_ENV_BIT":False, "MIN_BIT":4, "MAX_BIT":64}
 ALL_BIT = bit_boolean_dictionary["DATA_BIT"] and bit_boolean_dictionary["INDEX_BIT"] and bit_boolean_dictionary["IDP_BIT"] and bit_boolean_dictionary["COMPUTE_BIT"]  
-install_mode = 0  # are we using booleans are indices? 
+install_mode = 0  
 upgrade_mode = 0
 
-node_type_list = [] # empty node list
-
-# Do we need this function?
-# - This function is adding the keys from the dictionary into an empty list.
-#  I don't see it playing a major role since the empty list will hold the values
-#  obtained from the user/system arguements. 
+node_type_list = [] 
+ 
 def get_node_type():
     for key, value in bit_boolean_dictionary.items():
         if value:
@@ -64,13 +58,11 @@ def get_node_type():
 
 
 
-devel = esg_bash2py.Expand.colonMinus("devel", True)  # Create variable "devel" and set value to "true"?
-recommended_setup = 1 # booleans or indices?
+devel = esg_bash2py.Expand.colonMinus("devel", True) 
+recommended_setup = 1 
 custom_setup = 0
 use_local_files = 0
 
-
-# Installer basic info
 progname = "esg-node"
 script_version = "v2.0-RC5.4.0-devel"
 script_maj_version = "2.0"
@@ -82,7 +74,7 @@ force_install = False
 # User Defined / Settable (public)
 #--------------
 # install_prefix=${install_prefix:-${ESGF_INSTALL_PREFIX:-"/usr/local"}}
-install_prefix = esg_bash2py.Expand.colonMinus( q
+install_prefix = esg_bash2py.Expand.colonMinus(
     config.install_prefix, esg_bash2py.Expand.colonMinus("ESGF_INSTALL_PREFIX", "/usr/local"))
 #--------------
 
@@ -90,7 +82,6 @@ install_prefix = esg_bash2py.Expand.colonMinus( q
 
 esg_root_id = esg_functions.get_esg_root_id() 
 
-# Display Intaller guide
 def esgf_node_info():
 
     print '''
@@ -167,10 +158,6 @@ def esgf_node_info():
 
     '''
 
-
-## How is this function selecting the best mirror to use? 
-## Aren't the mirrors hardcoded in the get_esgf_dist_mirror function 
-## inside the esg_mirror_manager.py?
 def select_distribution_mirror(install_type):
      # Determining ESGF distribution mirror
     logger.info("before selecting distribution mirror: %s", config.config_dictionary["esgf_dist_mirror"])
@@ -183,9 +170,8 @@ def select_distribution_mirror(install_type):
 
     logger.info("selected distribution mirror: %s", config.config_dictionary["esgf_dist_mirror"])
 
-## Talk about in next meeting
 def set_esg_dist_url():
-     # # Setting esg_dist_url with previously gathered information
+    # Setting esg_dist_url with previously gathered information
     esg_dist_url_root = os.path.join("http://", config.config_dictionary["esgf_dist_mirror"], "dist")
     logger.debug("esg_dist_url_root: %s", esg_dist_url_root)
     if devel is True:
@@ -197,7 +183,7 @@ def set_esg_dist_url():
 
 
 def download_esg_installarg(esg_dist_url):
-    # # Downloading esg-installarg file
+    # Downloading esg-installarg file
     if not os.path.isfile(config.config_dictionary["esg_installarg_file"]) or force_install or os.path.getmtime(config.config_dictionary["esg_installarg_file"]) < os.path.getmtime(os.path.realpath(__file__)):
         esg_installarg_file_name = esg_bash2py.trim_string_from_head(config.config_dictionary["esg_installarg_file"])
         esg_functions.download_update(config.config_dictionary["esg_installarg_file"], os.path.join(esg_dist_url, "esgf-installer", esg_installarg_file_name), force_download=force_install)
@@ -208,11 +194,9 @@ def download_esg_installarg(esg_dist_url):
         except IOError, error:
             logger.error(error)
 
-# return a list of options 
 def create_new_list_from_keys(dictionary):
     return [node_option.split("_BIT")[0].lower() for node_option in dictionary.keys()]
 
-# compare system arguements with available options 
 def check_selected_node_type(bit_boolean_dictionary, node_type_list):
     ''' Make sure a valid node_type has been selected before performing and install '''
 
@@ -246,26 +230,9 @@ def check_selected_node_type(bit_boolean_dictionary, node_type_list):
             sys.exit(1)
     return True
 
-
-# example for comment below 
-# def user_answer():
-#   while True:
-#       begin_installation = raw_input("Are you ready to begin the installation? [Y/n] ") or default_install_answer
-
-#       if begin_installation.lower() == "n" or begin_installation.lower() == "no":
-#           print "Canceling installation"
-#           sys.exit(0)
-#       elif begin_installation.lower() == "y" or begin_installation.lower() == "yes":
-#           return "y"
-#       else:
-#           print "Invalid option.  Please select a valid option [Y/n]"
-
-
-
 def main(node_type_list):
-    esg_dist_url = "http://distrib-coffee.ipsl.jussieu.fr/pub/esgf/dist" # default distribution_url?
+    esg_dist_url = "http://distrib-coffee.ipsl.jussieu.fr/pub/esgf/dist"
     
-## an init() method can handle this 
     logger.info("esg-node initializing...")
     try:
         logger.info(socket.getfqdn())
@@ -273,7 +240,6 @@ def main(node_type_list):
         logger.error("Please be sure this host has a fully qualified hostname and reponds to socket.getfdqn() command")
         sys.exit()
 
-## lets make this a funcion that returns a value 
     # Determining if devel or master directory of the ESGF distribution mirror will be use for download of binaries
     if "devel" in script_version:
         logger.debug("Using devel version")
@@ -326,8 +292,6 @@ def main(node_type_list):
     default_install_answer = "Y"
     if devel is True:
         print "(Installing DEVELOPMENT tree...)"
-    ## this can turn into a standalone function - see comment above main for example
-    # the user_answer() function can simply return or break out the while loop
     while True:
         begin_installation = raw_input("Are you ready to begin the installation? [Y/n] ") or default_install_answer
         if begin_installation.lower() == "n" or begin_installation.lower() == "no":
@@ -340,8 +304,6 @@ def main(node_type_list):
 
     esg_setup.init_structure()
 
-# can be turned into a function that takes two parameters 
-# force_install and bit_boolean_dictionary and be used a checker
     if force_install:
         logger.info("(force install is ON)")
     # if node_type_bit & DATA_BIT != 0:

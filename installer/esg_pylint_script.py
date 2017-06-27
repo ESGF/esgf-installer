7c6c7214d27a2281767e8e0438ec368c21e839e6
Eliakin del Rosario <eking1566@gmail.com>
2017-06-27 16:29:51 -0700

    Script Modification

--- installer/esg_pylint_script.py
+++ installer/esg_pylint_script.py
@@ -6,32 +6,55 @@ import logging
 from pylint.lint import Run
 import pprint
 import esg_bash2py
+import getopt
+import sys
 
 logging.basicConfig(
     format="%(levelname): %(lineno)s %(funcName)s", level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
+# Create folder pylint_score_reports
 esg_bash2py.mkdir_p("pylint_score_reports")
 
+# get all files that contain the esg*_**.py pattern
 esgf_python_scripts = glob.glob("esg*_**.py")
-print "esgf_python_scripts:", esgf_python_scripts
-print "size of esgf_python_scripts: ", len(esgf_python_scripts)
+
 
 # file_name = raw_input("Enter the script name to lint:")
 # if file_name in esgf_python_scripts
 
-with open("pylint_score_reports/pylint_scores{date}.txt".format(date=time.strftime("%m_%d_%Y")), "a") as scores_file:
-    scores_file.write("\n")
-    scores_file.write(str(datetime.datetime.today())+"\n")
-    for script in esgf_python_scripts:
-        print "script name:", script
-        scores_file.write("script name: {script}".format(script=script)+"\n")
-        results = Run([script], exit=False)
-        try:
-            scores_file.write("score: "+str(results.linter.stats["global_note"])+"\n")
-        except KeyError, error:
-            logger.error(error)
-            logger.error("Can't find score for %s", script)
+def usage():
+    print """Usage: -h | -i <filename> | -all"""
 
+def main(argv):
+    try:
+        options, arguements = getopt.getopt(argv,"hi:all:")
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+
+    for option, arguements in options:
+        if option in ("-h", "--help"):
+            usage()
+            sys.exit(2)
+        elif option == "-all":
+
+            with open("pylint_score_reports/pylint_scores{date}.txt".format(date=time.strftime("%m_%d_%Y")), "a") as scores_file:
+                scores_file.write("\n")
+                scores_file.write(str(datetime.datetime.today())+"\n")
+                for script in esgf_python_scripts:
+                    print "script name:", script
+                    scores_file.write("script name: {script}".format(script=script)+"\n")
+                    results = Run([script], exit=False)
+                    try:
+                        scores_file.write("score: "+str(results.linter.stats["global_note"])+"\n")
+                    except KeyError, error:
+                        logger.error(error)
+                        logger.error("Can't find score for %s", script)
+
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
 
 

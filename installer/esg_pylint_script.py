193b93057d621f361a1136e814d2a175394faa1b
Eliakin del Rosario <eking1566@gmail.com>
2017-06-28 05:45:07 -0400

    Added functionality to scan one file only

--- installer/esg_pylint_script.py
+++ installer/esg_pylint_script.py
@@ -3,10 +3,9 @@ import os
 import datetime
 import time
 import logging
-from pylint.lint import Run
+#from pylint.lint import Run
 import pprint
-import esg_bash2py
-import getopt
+#import esg_bash2py
 import sys
 
 logging.basicConfig(
@@ -19,42 +18,59 @@ esg_bash2py.mkdir_p("pylint_score_reports")
 # get all files that contain the esg*_**.py pattern
 esgf_python_scripts = glob.glob("esg*_**.py")
 
-
-# file_name = raw_input("Enter the script name to lint:")
-# if file_name in esgf_python_scripts
-
 def usage():
-    print """Usage: -h | -i <filename> | -all"""
+    ''' Print proper usage message.'''
+    print ("""Usage: esg_python_script [-h|-i <filename>|-all]""")
+
+def scan_all():
+    ''' Scan all python modules and append score to a file.'''
+    with open("pylint_score_reports/pylint_scores{date}.txt".format(date=time.strftime("%m_%d_%Y")), "a") as scores_file:
+        scores_file.write("\n")
+        scores_file.write(str(datetime.datetime.today())+"\n")
+        for script in esgf_python_scripts:
+            print "script name:", script
+            scores_file.write("script name: {script}".format(script=script)+"\n")
+            results = Run([script], exit=False)
+            try:
+                scores_file.write("score: "+str(results.linter.stats["global_note"])+"\n")
+            except KeyError, error:
+                logger.error(error)
+                logger.error("Can't find score for %s", script)
+def scan_one(filename):
+    ''' Scan specified python modules and append score to a file with module name.'''
+     with open("pylint_score_reports/pylint_scores_{filename}_{date}.txt".format(filename=filename,date=time.strftime("%m_%d_%Y")), "a") as scores_file:
+        scores_file.write("\n")
+        scores_file.write(str(datetime.datetime.today())+"\n")
+        for script in esgf_python_scripts:
+            print "script name:", script
+            scores_file.write("script name: {script}".format(script=script)+"\n")
+            results = Run([script], exit=False)
+            try:
+                scores_file.write("score: "+str(results.linter.stats["global_note"])+"\n")
+            except KeyError, error:
+                logger.error(error)
+                logger.error("Can't find score for %s", script)
 
 def main(argv):
-    try:
-        options, arguements = getopt.getopt(argv,"hi:all:")
-    except getopt.GetoptError:
+    ''' Handle main program logic.'''
+    if len(argv) < 2:
         usage()
-        sys.exit(2)
+        sys.exit(1)
 
-    for option, arguements in options:
-        if option in ("-h", "--help"):
-            usage()
-            sys.exit(2)
-        elif option == "-all":
-
-            with open("pylint_score_reports/pylint_scores{date}.txt".format(date=time.strftime("%m_%d_%Y")), "a") as scores_file:
-                scores_file.write("\n")
-                scores_file.write(str(datetime.datetime.today())+"\n")
-                for script in esgf_python_scripts:
-                    print "script name:", script
-                    scores_file.write("script name: {script}".format(script=script)+"\n")
-                    results = Run([script], exit=False)
-                    try:
-                        scores_file.write("score: "+str(results.linter.stats["global_note"])+"\n")
-                    except KeyError, error:
-                        logger.error(error)
-                        logger.error("Can't find score for %s", script)
-
-
+    if argv[1] in ("-h", "--help"):
+        usage()
+        sys.exit(1)
+    elif argv[1] in ('-a','--all'):
+        print("Scanning all files...")
+        scan_all()
+    elif argv[1] == "-i":
+        filename = argv[2]
+        scan_one(filename)
+    else:
+        print("Cannot recognize '{}'".format(argv[1]))
+        usage()
 
 if __name__ == "__main__":
-    sys.exit(main(sys.argv[1:]))
+    sys.exit(main(sys.argv))
 
 

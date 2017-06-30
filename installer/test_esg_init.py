24ffb911586d198f043c4b28e328f2c0395a2568
Eliakin del Rosario <eking1566@gmail.com>
2017-06-30 07:58:14 -0700

    Changed test method.

--- installer/test_esg_init.py
+++ installer/test_esg_init.py
@@ -2,16 +2,14 @@
 
 import unittest
 import esg_init
-import esg_version_manager
+#import esg_version_manager
 
 
 class Test_ESG_Init(unittest.TestCase):
-    def setup(self):
-        self.var = ["apache_frontend_version", "cdat_version"]
     def test_init(self):
         for x in esg_init.init():
             try:
-                self.assertTrue(x in self.var, True)
+                self.assertTrue(x in ["apache_frontend_version", "cdat_version"], True)
             except TypeError, e:
                 print "error:", e
                 print "x:", x

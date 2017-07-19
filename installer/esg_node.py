b95c3b3703c69a11b05d09319ead0434682aabde
William Hill <hill119@llnl.gov>
2017-07-19 10:03:33 -0700

    fixed typos

--- installer/esg_node.py
+++ installer/esg_node.py
@@ -343,7 +343,7 @@ def main(node_type_list):
 
     # process command line arguments
     node_type_list = esg_cli_argument_manager.get_previous_node_type_config(
-        config.esg_config_type_file)
+        config["esg_config_type_file"])
     logger.debug("node_type_list: %s", node_type_list)
     esg_cli_argument_manager.process_arguments(
         install_mode, upgrade_mode, node_type_list, devel, esg_dist_url)

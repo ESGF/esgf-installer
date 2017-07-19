6e9d9d1dc1f02f637be0ed1aa25600a3a2f60db8
William Hill <hill119@llnl.gov>
2017-07-19 09:17:30 -0700

    added datetime module

--- installer/esg_setup.py
+++ installer/esg_setup.py
@@ -14,6 +14,7 @@ import grp
 import shlex
 import hashlib
 import urlparse
+import datetime
 from time import sleep
 from esg_init import EsgInit
 from esg_exceptions import UnprivilegedUserError, WrongOSError, UnverifiedScriptError

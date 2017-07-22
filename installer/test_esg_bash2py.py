3f20d4add5ce60ff516094abf00c8c671f801746
William Hill <hill119@llnl.gov>
2017-07-22 16:47:09 -0700

    added test_esg_bash2py.py

---
+++ installer/test_esg_bash2py.py
@@ -0,0 +1,24 @@
+#!/usr/bin/local/env python
+
+import unittest
+import esg_bash2py
+import os
+import yaml
+
+with open('esg_config.yaml', 'r') as config_file:
+    config = yaml.load(config_file)
+
+
+class test_ESG_bash2py(unittest.TestCase):
+
+    def test_trim_string_from_head(self):
+        output = esg_bash2py.trim_string_from_head("/usr/local/bin/esg_installarg_file")
+        self.assertEqual(output, "esg_installarg_file")
+
+    def test_trim_string_from_tail(self):
+        output = esg_bash2py.trim_string_from_tail("8.0.33")
+        self.assertEqual(output, "8")
+
+
+if __name__ == '__main__':
+    unittest.main()

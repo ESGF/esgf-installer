9c48f30d63d5b8716e00624a51ed522c2e4b4433
William Hill <hill119@llnl.gov>
2017-07-22 21:44:50 -0700

    added open log file

--- installer/esg_logging_manager.py
+++ installer/esg_logging_manager.py
@@ -9,7 +9,7 @@ def create_rotating_log(name, path=PATH):
     Creates a rotating log
     """
     logger = logging.getLogger(name)
-    coloredlogs.install(level='DEBUG', logger=logger, stream="esgf_colored_log.out")
+
 
     # add a rotating handler
     handler = RotatingFileHandler(path, maxBytes=10*1024*1024,
@@ -17,7 +17,8 @@ def create_rotating_log(name, path=PATH):
 
     # create formatter
     formatter = logging.Formatter("%(levelname)s - %(filename)s - %(lineno)s - %(funcName)s - %(asctime)s - %(message)s", datefmt='%m/%d/%Y %I:%M:%S %p')
-
+    colored_log_file = open("esgf_colored_log.out", "w")
+    coloredlogs.install(level='DEBUG', logger=logger, stream=colored_log_file)
     # add formatter to handler
     handler.setFormatter(formatter)
     handler.setLevel(logging.DEBUG)

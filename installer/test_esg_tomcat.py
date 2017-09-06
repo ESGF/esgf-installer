e96c890967b293ff26b039b2b68ba59c840b5ed7
William Hill <hill119@llnl.gov>
2017-09-06 13:52:55 -0700

    added esg_tomcat unit tests

---
+++ installer/test_esg_tomcat.py
@@ -0,0 +1,35 @@
+#!/usr/bin/local/env python
+
+import unittest
+import esg_tomcat_manager
+import os
+import shutil
+import yaml
+
+with open('esg_config.yaml', 'r') as config_file:
+    config = yaml.load(config_file)
+
+class test_ESG_tomcat(unittest.TestCase):
+
+    def tearDown(self):
+        os.remove("/tmp/apache-tomcat-8.5.20.tar.gz")
+        try:
+            shutil.rmtree("/usr/local/tomcat")
+        except OSError, error:
+            print "error:", error
+            pass
+
+    # def test_download_tomcat(self):
+    #     esg_tomcat_manager.download_tomcat()
+    #     self.assertTrue(os.path.isfile("/tmp/apache-tomcat-8.5.20.tar.gz"))
+
+    def test_main(self):
+        esg_tomcat_manager.main()
+        self.assertTrue(os.path.isdir("/usr/local/tomcat"))
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()

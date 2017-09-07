6e9b32c9609c660f40783076f7f3f8f0601de7cc
William Hill <hill119@llnl.gov>
2017-09-06 21:47:35 -0700

    added unit test

---
+++ installer/test_esg_apache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/local/env python
+
+import unittest
+import esg_apache_manager
+import esg_functions
+import os
+import shutil
+import yaml
+
+with open('esg_config.yaml', 'r') as config_file:
+    config = yaml.load(config_file)
+
+class test_ESG_apache(unittest.TestCase):
+
+    @classmethod
+    def tearDownClass(cls):
+        esg_functions.stream_subprocess_output("yum remove -y httpd")
+
+    def test_install_apache_httpd(self):
+        esg_apache_manager.install_apache_httpd()
+        output = esg_functions.call_subprocess("httpd -version")
+        print "output:", output
+        self.assertIsNotNone(output)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()

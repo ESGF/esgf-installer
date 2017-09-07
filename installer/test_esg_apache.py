000c6fbc72fedd6407620f8fc1b5bce70c395ffb
William Hill <hill119@llnl.gov>
2017-09-07 14:32:18 -0700

    added more assertions

--- installer/test_esg_apache.py
+++ installer/test_esg_apache.py
@@ -6,15 +6,18 @@ import esg_functions
 import os
 import shutil
 import yaml
+import pip
 
 with open('esg_config.yaml', 'r') as config_file:
     config = yaml.load(config_file)
 
 class test_ESG_apache(unittest.TestCase):
 
-    # @classmethod
-    # def tearDownClass(cls):
-    #     esg_functions.stream_subprocess_output("yum remove -y httpd")
+    @classmethod
+    def tearDownClass(cls):
+        esg_functions.stream_subprocess_output("yum remove -y httpd")
+        pip.main(["uninstall", "mod_wsgi"])
+
 
     def test_install_apache_httpd(self):
         esg_apache_manager.install_apache_httpd()
@@ -26,6 +29,12 @@ class test_ESG_apache(unittest.TestCase):
         esg_apache_manager.install_mod_wsgi()
         self.assertTrue(os.path.isfile("/etc/httpd/modules/mod_wsgi-py27.so"))
 
+
+        esg_apache_manager.make_python_eggs_dir()
+        self.assertTrue(os.path.isdir("/var/www/.python-eggs"))
+        owner, group = esg_functions.get_dir_owner_and_group("/var/www/.python-eggs")
+        self.assertEqual(owner, "apache")
+        self.assertEqual(group, "apache")
 
 
 

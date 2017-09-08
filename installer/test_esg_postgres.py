a6c302c8388301bc56f4d2de1870e5a3c0093b85
William Hill <hill119@llnl.gov>
2017-09-08 13:42:47 -0700

    added test case

--- installer/test_esg_postgres.py
+++ installer/test_esg_postgres.py
@@ -11,15 +11,19 @@ with open('esg_config.yaml', 'r') as config_file:
 
 class test_ESG_postgres(unittest.TestCase):
 
-    def test_check_postgress_process(self):
-        esg_postgres.stop_postgress()
-        output = esg_postgres.check_postgress_process()
-        self.assertEqual(output, 1)
+    # def test_check_for_postgres_db_user(self):
+    #     esg_postgres.stop_postgress()
+    #     esg_postgres.check_for_postgres_db_user()
 
-        esg_postgres.start_postgress()
-        output = esg_postgres.check_postgress_process()
-        self.assertEqual(output, 0)
-
+    def test_connect_to_db(self):
+        conn = esg_postgres.connect_to_db("postgres","postgres")
+        cur = conn.cursor()
+        cur.execute("""SELECT datname from pg_database;""")
+        rows = cur.fetchall()
+        print "\nRows: \n"
+        for row in rows:
+            print "   ", row[1]
+        assertIsNotNone(rows)
 
 
 if __name__ == '__main__':

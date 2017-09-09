f35f40584c3f40a52173fa60855c579d1fc732f4
William Hill <hill119@llnl.gov>
2017-09-08 22:37:46 -0700

    updated tests

--- installer/test_esg_postgres.py
+++ installer/test_esg_postgres.py
@@ -23,6 +23,9 @@ class test_ESG_postgres(unittest.TestCase):
         cur = conn.cursor()
         cur.execute("DROP USER testuser;")
         cur.execute("DROP DATABASE IF EXISTS unittestdb;")
+        cur.execute("DROP USER dbsuper;")
+        cur.execute("DROP USER esgcet;")
+        cur.execute("DROP DATABASE IF EXISTS esgcet;")
         conn.close()
 
     def test_connect_to_db(self):
@@ -58,19 +61,32 @@ class test_ESG_postgres(unittest.TestCase):
         print users
         self.assertIsNotNone(users)
 
+    def test_list_users(self):
+        user_list = esg_postgres.list_users("postgres", "postgres")
+        self.assertIsNotNone(user_list)
+
     def test_list_schemas(self):
         output = esg_postgres.postgres_list_db_schemas("postgres", "postgres")
         self.assertIsNotNone(output)
 
     def test_add_schema_from_file(self):
+        user_list = esg_postgres.list_users("postgres", "postgres")
         conn = esg_postgres.connect_to_db("postgres","postgres")
+        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
         cur = conn.cursor()
-        try:
+        if "dbsuper" not in user_list:
             cur.execute("CREATE USER dbsuper with CREATEROLE superuser PASSWORD 'password';")
+        if "esgcet" not in user_list:
             cur.execute("CREATE USER esgcet PASSWORD 'password';")
             cur.execute("CREATE DATABASE esgcet;")
-        except Exception, error:
-            print "error:", error
+            cur.execute("GRANT dbsuper TO esgcet;")
+        # try:
+        #     #cur.execute("CREATE USER dbsuper with CREATEROLE superuser PASSWORD 'password';")
+        #     #cur.execute("CREATE USER esgcet PASSWORD 'password';")
+        #     #cur.execute("CREATE DATABASE esgcet;")
+        #     cur.execute("GRANT dbsuper TO esgcet;")
+        # except Exception, error:
+        #     print "error:", error
         conn.commit()
         conn.close()
 
@@ -78,9 +94,13 @@ class test_ESG_postgres(unittest.TestCase):
         cur2 = conn2.cursor()
         try:
             # cur.execute("postgres < sqldata/esgf_esgcet.sql")
+            schema_tables = esg_postgres.postgres_list_schemas_tables("esgcet", "esgcet")
+            print "schema_tables after: ", schema_tables
             cur2.execute(open("sqldata/esgf_esgcet.sql", "r").read())
             output = esg_postgres.postgres_list_db_schemas("esgcet", "esgcet")
             print "output after add schema:", output
+            schema_tables = esg_postgres.postgres_list_schemas_tables("esgcet", "esgcet")
+            print "schema_tables after: ", schema_tables
         except Exception, error:
             print 'error:', error
 

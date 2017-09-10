61072b095db4bb4c94927028fe1e04f96292a194
William Hill <hill119@llnl.gov>
2017-09-09 23:06:26 -0700

    fixed test

--- installer/test_esg_postgres.py
+++ installer/test_esg_postgres.py
@@ -69,6 +69,11 @@ class test_ESG_postgres(unittest.TestCase):
         user_list = esg_postgres.list_users("postgres", "postgres")
         self.assertIsNotNone(user_list)
 
+        conn = esg_postgres.connect_to_db("postgres","postgres")
+        user_list = esg_postgres.list_users(conn=conn)
+        print "user_list with conn test:", user_list
+        self.assertIsNotNone(user_list)
+
     def test_list_schemas(self):
         output = esg_postgres.postgres_list_db_schemas("postgres", "postgres")
         self.assertIsNotNone(output)
@@ -84,41 +89,27 @@ class test_ESG_postgres(unittest.TestCase):
             cur.execute("CREATE USER esgcet PASSWORD 'password';")
             cur.execute("CREATE DATABASE esgcet;")
             cur.execute("GRANT dbsuper TO esgcet;")
-        # try:
-        #     #cur.execute("CREATE USER dbsuper with CREATEROLE superuser PASSWORD 'password';")
-        #     #cur.execute("CREATE USER esgcet PASSWORD 'password';")
-        #     #cur.execute("CREATE DATABASE esgcet;")
-        #     cur.execute("GRANT dbsuper TO esgcet;")
-        # except Exception, error:
-        #     print "error:", error
         conn.commit()
         conn.close()
 
         conn2 = esg_postgres.connect_to_db("esgcet","esgcet")
         cur2 = conn2.cursor()
         try:
-            # cur.execute("postgres < sqldata/esgf_esgcet.sql")
-            # schema_tables = esg_postgres.postgres_list_schemas_tables("esgcet", "esgcet")
-            # print "schema_tables: ", schema_tables
             cur2.execute("SELECT table_schema,table_name FROM information_schema.tables ORDER BY table_schema,table_name;")
             tables = cur2.fetchall()
             before_tables_list = [table[1]  for table in tables if table[0] == 'public']
             print "tables before:", before_tables_list
             print "tables before length:", len(before_tables_list)
             cur2.execute(open("sqldata/esgf_esgcet.sql", "r").read())
-            # conn.commit()
 
             cur2.execute("SELECT table_schema,table_name FROM information_schema.tables ORDER BY table_schema,table_name;")
             tables = cur2.fetchall()
             after_tables_list = [table[1] for table in tables if table[0] == 'public']
             print "tables after:", after_tables_list
             print "tables after length:", len(after_tables_list)
-            # output = esg_postgres.postgres_list_db_schemas("esgcet", "esgcet")
-            # print "output after add schema:", output
-            # schema_tables = esg_postgres.postgres_list_schemas_tables("esgcet", "esgcet")
-            # print "schema_tables after: ", schema_tables
         except Exception, error:
             print 'error:', error
+        conn.close()
         self.assertIsNotNone(after_tables_list)
 
 

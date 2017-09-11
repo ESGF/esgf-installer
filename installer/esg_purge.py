371c0d88460c3c1d868920d343b16c7b4a0ef7da
William Hill <hill119@llnl.gov>
2017-09-11 09:51:14 -0700

    created esg_purge.py

---
+++ installer/esg_purge.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+import subprocess
+import esg_functions
+
+def purge_postgres():
+    esg_functions.call_subprocess("yum remove -y postgresql postgresql-libs postgresql-server")
+    try:
+        shutil.rmtree("/usr/local/pgsql")
+        shutil.rmtree("/var/lib/pgsql")
+        os.remove(os.path.join(os.environ["HOME"], ".pgpass"))
+    except OSError, error:
+        print "error:", error
+
+
+def main():
+    purge_postgres()
+    pass
+
+if __name__ == '__main__':
+    main()

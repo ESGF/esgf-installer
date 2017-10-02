192f500edbf650997aa3971efbabc232836018a6
William Hill <hill119@llnl.gov>
2017-10-02 11:58:24 -0700

    added purge_tomcat()

--- installer/esg_purge.py
+++ installer/esg_purge.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import glob
 import subprocess
 import esg_functions
 
@@ -12,9 +13,26 @@ def purge_postgres():
     except OSError, error:
         print "error:", error
 
+def purge_tomcat():
+    # esg-node --stop may not actually cause Tomcat to exit properly,
+    # so force-kill all remaining instances
+    esg_functions.call_subprocess("pkill -9 -u tomcat")
+    shutil.rmtree("/etc/logrotate.d/esgf_tomcat")
+
+    tomcat_directories = glob.glob("/usr/local/tomcat*")
+    tomcat_directories.extend(glob.glob("/usr/local/apache-tomcat*"))
+    for directory in tomcat_directories:
+        try:
+            shutil.rmtree(directory)
+        except OSError, error:
+            print "error deleting directory:", error
+
+    # Tomcat may leave stuck java processes.  Kill them with extreme prejudice
+    esg_functions.call_subprocess("pkill -9 -f 'java.*/usr/local/tomcat'")
 
 def main():
     purge_postgres()
+    purge_tomcat()
     pass
 
 if __name__ == '__main__':

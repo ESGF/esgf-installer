3e1f7845776483512a1ca6e5ba15f6baf863dbcf
William Hill <hill119@llnl.gov>
2017-10-13 14:12:28 -0700

    code cleanup

--- installer/esg_tomcat_manager.py
+++ installer/esg_tomcat_manager.py
@@ -69,7 +69,6 @@ def download_tomcat():
         return False
     # tomcat_download_url = "http://mirror.reverse.net/pub/apache/tomcat/tomcat-8/v{TOMCAT_VERSION}/bin/apache-tomcat-{TOMCAT_VERSION}.tar.gz".format(
     tomcat_download_url = "http://archive.apache.org/dist/tomcat/tomcat-8/v8.5.20/bin/apache-tomcat-8.5.20.tar.gz"
-        # TOMCAT_VERSION=TOMCAT_VERSION)
     print "downloading Tomcat"
     r = requests.get(tomcat_download_url)
     with open("/tmp/apache-tomcat-{TOMCAT_VERSION}.tar.gz".format(TOMCAT_VERSION=TOMCAT_VERSION), "wb") as code:

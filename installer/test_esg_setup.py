3bcd4bc3963cccea8888a82d8e6feae87f2da735
William Hill <hill119@llnl.gov>
2017-10-16 15:34:11 -0700

    added assertions

--- installer/test_esg_setup.py
+++ installer/test_esg_setup.py
@@ -3,6 +3,9 @@
 import unittest
 import os
 import shutil
+import esg_functions
+from distutils.spawn import find_executable
+import sys
 import esg_setup
 
 
@@ -11,10 +14,19 @@ class test_ESG_Setup(unittest.TestCase):
 	@classmethod
 	def tearDownClass(cls):
 		os.remove("/tmp/Miniconda2-latest-Linux-x86_64.sh")
+		esg_functions.stream_subprocess_output("conda install -y anaconda-clean")
+		esg_functions.stream_subprocess_output("anaconda-clean --yes")
+		shutil.rmtree("/usr/local/testConda")
 
 	def test_download_conda(self):
-		esg_setup.download_conda()
+		print "path:", sys.path
+		esg_setup.download_conda("/usr/local/testConda")
 		self.assertTrue(os.path.isfile("/tmp/Miniconda2-latest-Linux-x86_64.sh"))
+		print "path after:", sys.path
+		self.assertTrue("/usr/local/testConda" in sys.path)
+		self.assertTrue(find_executable("conda"))
+
+
 
 
 if __name__ == '__main__':

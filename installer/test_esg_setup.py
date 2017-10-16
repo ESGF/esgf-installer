62d1aa46631925e6d46db6cafee30d35e98c4ebf
William Hill <hill119@llnl.gov>
2017-10-16 14:45:03 -0700

    added tearDown

--- installer/test_esg_setup.py
+++ installer/test_esg_setup.py
@@ -2,9 +2,15 @@
 
 import unittest
 import os
+import shutil
 import esg_setup
 
+
 class test_ESG_Setup(unittest.TestCase):
+
+	@classmethod
+	def tearDownClass(cls):
+		shutil.rmtree("/tmp/Miniconda2-latest-Linux-x86_64.sh")
 
 	def test_download_conda(self):
 		esg_setup.download_conda()

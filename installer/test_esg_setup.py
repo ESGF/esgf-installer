bea1b655e8cb4f0dc8b066681f7c701dce37120b
William Hill <hill119@llnl.gov>
2017-10-16 14:36:18 -0700

    added test_download_conda()

--- installer/test_esg_setup.py
+++ installer/test_esg_setup.py
@@ -1,16 +1,14 @@
 #!/usr/bin/local/env python
 
 import unittest
+import os
 import esg_setup
 
 class test_ESG_Setup(unittest.TestCase):
-	# def test_hello_world(self):
-	# 	print "Testing:", esg_setup.print_hello()
-	# 	self.assertEqual(esg_setup.print_hello(), "hello world")
 
-	def test_is_in_git(self):
-		output = esg_setup.is_in_git("/Users/hill119/Development/esgf-installer/installer/esg_init.py")
-		self.assertEqual(output, 0)
+	def test_download_conda(self):
+		esg_setup.download_conda()
+		self.assertTrue(os.path.isfile("/tmp/Miniconda2-latest-Linux-x86_64.sh"))
 
 
 if __name__ == '__main__':

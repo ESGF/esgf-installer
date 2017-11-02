8266320a8f5654beaf4697477f60dcfdb92a679c
William Hill <hill119@llnl.gov>
2017-11-02 09:52:21 -0700

    cleaned up create_self_signed_cert()

--- installer/esg_cert_manager.py
+++ installer/esg_cert_manager.py
@@ -582,36 +582,32 @@ def set_aside_web_app():
 def set_aside_web_app_cleanup():
     pass
 
-from OpenSSL import crypto, SSL
-from socket import gethostname
-from pprint import pprint
-from time import gmtime, mktime
-from os.path import exists, join
-
-CERT_FILE = "myapp.crt"
-KEY_FILE = "myapp.key"
 
 def create_self_signed_cert(cert_dir):
     """
     If datacard.crt and datacard.key don't exist in cert_dir, create a new
     self-signed cert and keypair and write them into that directory.
-    """
 
-    if not exists(join(cert_dir, CERT_FILE)) \
-            or not exists(join(cert_dir, KEY_FILE)):
+    Source: https://skippylovesmalorie.wordpress.com/2010/02/12/how-to-generate-a-self-signed-certificate-using-pyopenssl/
+    """
+    CERT_FILE = "myapp.crt"
+    KEY_FILE = "myapp.key"
+
+    if not os.path.exists(os.path.join(cert_dir, CERT_FILE)) \
+            or not os.path.exists(os.path.join(cert_dir, KEY_FILE)):
 
         # create a key pair
-        k = crypto.PKey()
-        k.generate_key(crypto.TYPE_RSA, 1024)
+        k = OpenSSL.crypto.PKey()
+        k.generate_key(OpenSSL.crypto.TYPE_RSA, 1024)
 
         # create a self-signed cert
-        cert = crypto.X509()
+        cert = OpenSSL.crypto.X509()
         cert.get_subject().C = "US"
-        cert.get_subject().ST = "Minnesota"
-        cert.get_subject().L = "Minnetonka"
-        cert.get_subject().O = "my company"
-        cert.get_subject().OU = "my organization"
-        cert.get_subject().CN = gethostname()
+        cert.get_subject().ST = "California"
+        cert.get_subject().L = "Livermore"
+        cert.get_subject().O = "LLNL"
+        cert.get_subject().OU = "ESGF"
+        cert.get_subject().CN = socket.gethostname()
         cert.set_serial_number(1000)
         cert.gmtime_adj_notBefore(0)
         cert.gmtime_adj_notAfter(10*365*24*60*60)
@@ -619,7 +615,7 @@ def create_self_signed_cert(cert_dir):
         cert.set_pubkey(k)
         cert.sign(k, 'sha1')
 
-        open(join(cert_dir, CERT_FILE), "wt").write(
-            crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
-        open(join(cert_dir, KEY_FILE), "wt").write(
-            crypto.dump_privatekey(crypto.FILETYPE_PEM, k))
+        open(os.path.join(cert_dir, CERT_FILE), "wt").write(
+            OpenSSL.crypto.dump_certificate(OpenSSL.crypto.FILETYPE_PEM, cert))
+        open(os.path.join(cert_dir, KEY_FILE), "wt").write(
+            OpenSSL.crypto.dump_privatekey(OpenSSL.crypto.FILETYPE_PEM, k))

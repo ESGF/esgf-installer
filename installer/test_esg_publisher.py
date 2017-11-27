de8aa26d83155e1af6e2405e589568a610c8d262
William Hill <hill119@llnl.gov>
2017-11-27 11:43:49 -0800

    added test_setup_publisher()

--- installer/test_esg_publisher.py
+++ installer/test_esg_publisher.py
@@ -1,5 +1,7 @@
 import unittest
 import esg_publisher
+import esg_postgres
+import esg_bash2py
 import os
 import shutil
 import yaml
@@ -9,12 +11,18 @@ with open('esg_config.yaml', 'r') as config_file:
 
 
 class test_ESG_publisher(unittest.TestCase):
+
+    def purge_publisher(self):
+        try:
+            shutil.rmtree("/tmp/esg-publisher")
+        except OSError, error:
+            print "Could not purge esg-publisher", error
+
     @classmethod
     def setUpClass(cls):
         print "\n*******************************"
         print "Setting up ESGF Publisher Test Fixture"
         print "******************************* \n"
-        pass
 
     @classmethod
     def tearDownClass(cls):
@@ -25,7 +33,6 @@ class test_ESG_publisher(unittest.TestCase):
             shutil.rmtree("/tmp/esg-publisher")
         except OSError, error:
             print "Error deleting /tmp/esg-publisher", error
-        pass
 
     def test_clone_publisher_repo(self):
         esg_publisher.clone_publisher_repo("/tmp/esg-publisher")
@@ -36,6 +43,11 @@ class test_ESG_publisher(unittest.TestCase):
         print "active branch:", branch.name
         self.assertEquals(branch.name, "devel")
 
+        self.purge_publisher()
+
+    def test_setup_publisher(self):
+        esg_publisher.setup_publisher()
+        self.assertTrue(os.path.isdir('/esg/config/esgcet/'))
     # def test_checkout_publisher_branch(self):
     #     repo = checkout_publisher_branch("/tmp/esg-publisher", "v3.2.7")
     #     branch = repo.active_branch
@@ -43,53 +55,31 @@ class test_ESG_publisher(unittest.TestCase):
     #     self.assertEquals(branch.name, "v3.2.7")
 
 
-    # def test_esgcet():
-    #     print '''
-    #     ----------------------------
-    #     ESGCET Test...
-    #     ----------------------------
-    #     '''
+    # def test_esgcet(self):
+    #     print "\n*******************************"
+    #     print "Publisher Test"
+    #     print "******************************* \n"
     #     starting_directory = os.getcwd()
     #     os.chdir(config["workdir"])
     #
-    #     esg_postgres.start_postgress()
+    #     esg_postgres.start_postgres()
     #
     #     esgcet_testdir = os.path.join(config[
     #                                   "thredds_root_dir"], "test")
-    #
-    #     try:
-    #         os.makedirs(esgcet_testdir)
-    #     except OSError, exception:
-    #         if exception.errno != 17:
-    #             raise
-    #         sleep(1)
-    #         pass
-    #     except Exception, exception:
-    #         print "Exception occurred when attempting to create the {esgcet_testdir} directory: {exception}".format(esgcet_testdir=esgcet_testdir, exception=exception)
-    #         esg_functions.exit_with_error(1)
+    #     esg_bash2py.mkdir_p(esgcet_testdir)
     #
     #     os.chown(esgcet_testdir, config[
     #              "installer_uid"], config["installer_gid"])
     #
-    #     try:
-    #         os.mkdir(config["thredds_replica_dir"])
-    #     except OSError, exception:
-    #         if exception.errno != 17:
-    #             raise
-    #         sleep(1)
-    #         pass
-    #     except Exception, exception:
-    #         print "Exception occurred when attempting to create the {esgcet_testdir} directory: {exception}".format(esgcet_testdir=esgcet_testdir, exception=exception)
-    #         esg_functions.exit_with_error(1)
+    #     esg_bash2py.mkdir_p(config["thredds_replica_dir"])
     #
     #     os.chown(config["thredds_replica_dir"], config[
     #              "installer_uid"], config["installer_gid"])
     #     print "esgcet test directory: [%s]" % esgcet_testdir
     #
     #     fetch_file = "sftlf.nc"
-    #     if esg_functions.checked_get(os.path.join(esgcet_testdir, fetch_file), "http://" + config["esg_dist_url_root"] + "/externals/" + fetch_file) > 0:
+    #     if not esg_functions.download_update(os.path.join(esgcet_testdir, fetch_file), "http://" + config["esg_dist_url_root"] + "/externals/" + fetch_file):
     #         print " ERROR: Problem pulling down %s from esg distribution" % (fetch_file)
-    #         os.chdir(starting_directory)
     #         esg_functions.exit_with_error(1)
     #
     #     # Run test...

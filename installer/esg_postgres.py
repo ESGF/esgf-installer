dac04e351240c771a9020e54aa6c23e4d8a75a5b
William Hill <hill119@llnl.gov>
2017-11-27 14:34:30 -0800

    added install of postgres96-devel

--- installer/esg_postgres.py
+++ installer/esg_postgres.py
@@ -34,7 +34,7 @@ def download_postgres():
         config.write()
 
     esg_functions.stream_subprocess_output("rpm -Uvh https://yum.postgresql.org/9.6/redhat/rhel-6-x86_64/pgdg-redhat96-9.6-3.noarch.rpm")
-    esg_functions.stream_subprocess_output("yum -y install postgresql96-server postgresql96")
+    esg_functions.stream_subprocess_output("yum -y install postgresql96-server postgresql96 postgresql96-devel")
 
 def initialize_postgres():
     try:

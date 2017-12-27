87406b016bca4a24e1bbdecb5a146c01f6eb5a40
William Hill <hill119@llnl.gov>
2017-12-26 18:07:07 -0800

    fixed typo

--- installer/esg_subsystem.py
+++ installer/esg_subsystem.py
@@ -111,7 +111,7 @@ def setup_orp():
 
     if os.path.isdir("/usr/local/tomcat/webapps/esg-orp"):
         if esg_property_manager.get_property("install_orp"):
-            orp_install = esg_property_manager.get_property("setup_orp")
+            orp_install = esg_property_manager.get_property("install_orp")
         else:
             orp_install = raw_input("Existing ORP installation found.  Do you want to continue with the ORP installation [y/N]: ") or "no"
         if orp_install.lower() in ["no", "n"]:

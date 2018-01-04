d7587b6c9902f36130f9ad6298e79542ee6be142
William Hill <hill119@llnl.gov>
2018-01-04 10:55:59 -0800

    removed old code

--- installer/esg_subsystem.py
+++ installer/esg_subsystem.py
@@ -27,65 +27,6 @@ logger = esg_logging_manager.create_rotating_log(__name__)
 with open(os.path.join(os.path.dirname(__file__), 'esg_config.yaml'), 'r') as config_file:
     config = yaml.load(config_file)
 
-CATALINA_HOME = "/usr/local/tomcat"
-
-def setup_subsystem(subsystem, distribution_directory, esg_dist_url, force_install=False):
-    '''
-    arg (1) - name of installation script root name. Ex:security which resolves to script file esg-security
-    arg (2) - directory on the distribution site where script is fetched from Ex: orp
-    usage: setup_subsystem security orp - looks for the script esg-security in the distriubtion dir orp
-    '''
-
-    subsystem_install_script_path = os.path.join(config["scripts_dir"],"esg-{subsystem}".format(subsystem=subsystem))
-
-    #---
-    #check that you have at one point in time fetched the subsystem's installation script
-    #if indeed you have we will assume you would like to proceed with setting up the latest...
-    #Otherwise we just ask you first before you pull down new code to your machine...
-    #---
-
-    if force_install:
-        default = "y"
-    else:
-        default = "n"
-
-    if os.path.exists(subsystem_install_script_path) or force_install:
-        if default.lower() in ["y", "yes"]:
-            run_installation = raw_input("Would you like to set up {subsystem} services? [Y/n]: ".format(subsystem=subsystem)) or "y"
-        else:
-            run_installation = raw_input("Would you like to set up {subsystem} services? [y/N]: ".format(subsystem=subsystem)) or "n"
-
-        if run_installation.lower() in ["n", "no"]:
-            print "Skipping installation of {subsystem}".format(subsystem=subsystem)
-            return True
-
-    print "-------------------------------"
-    print "LOADING installer for {subsystem}... ".format(subsystem=subsystem)
-    esg_bash2py.mkdir_p(config["workdir"])
-    with esg_bash2py.pushd(config["workdir"]):
-        logger.debug("Changed directory to %s", os.getcwd())
-
-        with esg_bash2py.pushd(config["scripts_dir"]):
-            logger.debug("Changed directory to %s", os.getcwd())
-
-            subsystem_full_name = "esg-{subsystem}".format(subsystem=subsystem)
-            subsystem_remote_url = "{esg_dist_url}/{distribution_directory}/{subsystem_full_name}".format(esg_dist_url=esg_dist_url, distribution_directory=distribution_directory, subsystem_full_name=subsystem_full_name)
-            if not esg_functions.download_update("{subsystem_full_name}".format(subsystem_full_name=subsystem_full_name), subsystem_remote_url):
-                logger.error("Could not download %s", subsystem_full_name)
-                return False
-            try:
-                os.chmod(subsystem_full_name, 0755)
-            except OSError:
-                logger.exception("Unable to change permissions on %s", subsystem_full_name)
-
-
-    logger.info("script_dir contents: %s", os.listdir(config["scripts_dir"]))
-    subsystem_underscore = subsystem.replace("-", "_")
-    execute_subsystem_command = ". {scripts_dir}/{subsystem_full_name}; setup_{subsystem_underscore}".format(scripts_dir=config["scripts_dir"], subsystem_full_name=subsystem_full_name, subsystem_underscore=subsystem_underscore)
-    setup_subsystem_process = subprocess.Popen(['bash', '-c', execute_subsystem_command])
-    setup_subsystem_stdout, setup_subsystem_stderr = setup_subsystem_process.communicate()
-    logger.debug("setup_subsystem_stdout: %s", setup_subsystem_stdout)
-    logger.debug("setup_subsystem_stderr: %s", setup_subsystem_stderr)
 
 def download_orp_war(orp_url):
 

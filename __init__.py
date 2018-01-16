e1f156ac0b2ee47660fb173b78c143d30cf2d5f1
William Hill <hill119@llnl.gov>
2018-01-16 13:22:52 -0800

    moved logging to different file

--- __init__.py
+++
@@ -1,7 +0,0 @@
-import logging
-# import coloredlogs
-from logging.handlers import RotatingFileHandler
-
-
-print "Initializing loggers"
-logging.basicConfig(format="%(levelname)s - %(filename)s - %(lineno)s - %(funcName)s - %(asctime)s - %(message)s", datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)

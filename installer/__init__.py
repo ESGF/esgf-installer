75b71deb22ea2e1183d30614f4dbc82e3e53d2b7
William Hill <hill119@llnl.gov>
2018-01-16 13:28:27 -0800

    specified stream

--- installer/__init__.py
+++ installer/__init__.py
@@ -4,4 +4,4 @@ from logging.handlers import RotatingFileHandler
 
 
 print "Initializing loggers"
-logging.basicConfig(format="%(levelname)s - %(filename)s - %(lineno)s - %(funcName)s - %(asctime)s - %(message)s", datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
+logging.basicConfig(format="%(levelname)s - %(filename)s - %(lineno)s - %(funcName)s - %(asctime)s - %(message)s", datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG, stream=sys.stdout)

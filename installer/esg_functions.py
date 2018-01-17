7db084edd9623f1a29ac3dd54e8d0a928585d4d4
William Hill <hill119@llnl.gov>
2018-01-17 10:39:38 -0800

    added print statement

--- installer/esg_functions.py
+++ installer/esg_functions.py
@@ -32,6 +32,7 @@ import esg_logging_manager
 
 # logger = esg_logging_manager.create_rotating_log(__name__)
 logger = logging.getLogger(__name__)
+print "esg_functions logger:", logger
 
 with open(os.path.join(os.path.dirname(__file__), 'esg_config.yaml'), 'r') as config_file:
     config = yaml.load(config_file)

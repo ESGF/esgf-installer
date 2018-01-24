4bdffc7518703f0ced78a839f3e8df457a2c54a6
William Hill <hill119@llnl.gov>
2018-01-24 15:54:32 -0800

    deleted installer directory

--- installer/__init__.py
+++
@@ -1,8 +0,0 @@
-import logging
-import sys
-# import coloredlogs
-from logging.handlers import RotatingFileHandler
-
-
-print "Initializing loggers"
-logging.basicConfig(format="%(levelname)s - %(filename)s - %(lineno)s - %(funcName)s - %(asctime)s - %(message)s", datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG, stream=sys.stdout)

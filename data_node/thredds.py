52abf9801cf06dfc58e408d59e55a52c35cf0a72
William Hill <hill119@llnl.gov>
2018-01-24 11:29:37 -0800

    log password hash

--- data_node/thredds.py
+++ data_node/thredds.py
@@ -54,6 +54,7 @@ def create_password_hash(tomcat_user_password):
     '''Creates a hash for a Tomcat user's password using Tomcat's digest.sh script'''
     password_hash = esg_functions.call_subprocess("/usr/local/tomcat/bin/digest.sh -a SHA {tomcat_user_password}".format(tomcat_user_password=tomcat_user_password))
     print "password hash:",  password_hash["stdout"]
+    logger.info("password hash: %s", password_hash["stdout"].split(":")[1])
     return password_hash["stdout"].split(":")[1]
 
 def update_tomcat_users_file(tomcat_username, password_hash, tomcat_users_file=config["tomcat_users_file"]):

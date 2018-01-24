a7d47fd179d262b2cff2d70cd07ed89551deb2c2
William Hill <hill119@llnl.gov>
2018-01-24 15:27:09 -0800

    added context.py

--- tests/test_esg_version_manager.py
+++ tests/test_esg_version_manager.py
@@ -2,7 +2,7 @@
 
 import unittest
 import os
-from esgf_utilities import esg_version_manager
+from context import esgf_utilities
 import semver
 import yaml
 
@@ -17,26 +17,26 @@ class test_ESG_version_manager(unittest.TestCase):
         # output = esg_version_manager.check_module_version("esgcet", "3.0.1")
         # self.assertEqual(output,0)
 
-        output = esg_version_manager.check_module_version("pylint", "1.9")
+        output = esgf_utilities.esg_version_manager.check_module_version("pylint", "1.9")
         self.assertEqual(output,1)
 
     def test_get_current_esgf_library_version(self):
-        output = esg_version_manager.get_current_esgf_library_version("esgf-security")
+        output = esgf_utilities.esg_version_manager.get_current_esgf_library_version("esgf-security")
         self.assertEqual(output, True)
 
     def test_get_current_webapp_version(self):
-        output = esg_version_manager.get_current_webapp_version("esg-orp")
+        output = esgf_utilities.esg_version_manager.get_current_webapp_version("esg-orp")
         self.assertEqual(output, "2.9.0")
 
     def test_check_webapp_version(self):
-        output = esg_version_manager.check_webapp_version("esg-orp", "2.0")
+        output = esgf_utilities.esg_version_manager.check_webapp_version("esg-orp", "2.0")
         self.assertEqual(output, 0)
 
-        output = esg_version_manager.check_webapp_version("esg-orp", "4.0")
+        output = esgf_utilities.esg_version_manager.check_webapp_version("esg-orp", "4.0")
         self.assertEqual(output, 1)
 
     def test_compare_versions(self):
-        self.assertTrue(esg_version_manager.compare_versions("1.8.0_131", "1.8.0_111"))
+        self.assertTrue(esgf_utilities.esg_version_manager.compare_versions("1.8.0_131", "1.8.0_111"))
 
 
 if __name__ == '__main__':

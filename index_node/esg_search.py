da66db0eaabf00656e49de87670c8405bd4be5ef
William Hill <hill119@llnl.gov>
2018-03-05 12:56:43 -0800

    added imports, setup main function

--- index_node/esg_search.py
+++ index_node/esg_search.py
@@ -1,112 +1,23 @@
 import os
 import zipfile
 import logging
+import shutil
+import filecmp
+import ConfigParser
 import yaml
 import requests
 from clint.textui import progress
+from git import Repo
 from esgf_utilities import esg_functions
 from esgf_utilities import esg_bash2py
+from esgf_utilities import esg_property_manager
+from esgf_utilities import esg_version_manager
+from base import esg_tomcat_manager
+import solr
 
 
 with open(os.path.join(os.path.dirname(__file__), os.pardir, 'esg_config.yaml'), 'r') as config_file:
     config = yaml.load(config_file)
-
-
-
-
-
-DEBUG=${DEBUG:-0}
-VERBOSE=${VERBOSE:-0}
-
-#--------------
-#User Defined / Settable (public)
-#--------------
-install_prefix=${install_prefix:-"/usr/local"}
-esg_solr_user=${SOLR_USER:-"solr"}
-esg_solr_group=${SOLR_GROUP:-"solr"}
-esg_functions_file=${install_prefix}/bin/esg-functions
-esg_root_dir=${esg_root_dir:-"/esg"}
-esg_config_dir=${esg_config_dir:-"${esg_root_dir}/config"}
-tomcat_install_dir=${tomcat_install_dir:-${CATALINA_HOME:=${install_prefix}/tomcat}}
-workdir=${workdir:-~/workbench/esg}
-install_manifest=${install_manifest:-"${esg_root_dir}/esgf-install-manifest"}
-esgf_shards_config_file=${esg_config_dir}/esgf_shards.config
-esgf_shards_static_file=${esg_config_dir}/esgf_shards_static.xml
-esgf_shards_dynamic_file=${esg_config_dir}/esgf_shards.xml
-esgf_excludes_file=${esg_config_dir}/esgf_excludes.txt
-
-esgf_publisher_resources_home=${esgf_publisher_resources_home:-${esgf_config_dir}}
-esgf_publisher_resources_repo=${esgf_publisher_resources_repo:-https://github.com/ESGF/esgf-publisher-resources.git}
-#--------------
-
-date_format=${date_format:-"+%Y_%m_%d_%H%M%S"}
-compress_extensions=${compress_extensions:-".tar.gz|.tar.bz2|.tgz|.bz2"}
-force_install=${force_install:-0}
-
-installer_uid=${installer_uid:-$(id ${HOME##*/} | sed 's/.*uid=\([0-9]*\).*/\1/')}
-[ $? != 0 ] || [ -z "$installer_uid" ] && echo "installer_uid is not set" && exit 1
-installer_gid=${installer_gid:-$(id ${HOME##*/} | sed 's/.*gid=\([0-9]*\).*/\1/')}
-[ $? != 0 ] || [ -z "$installer_gid" ] && echo "installer_gid is not set" && exit 1
-installer_home=${HOME}
-
-esg_search_version=${esg_search_version:-"1.2.0"}
-search_install_dir=${ESGF_INSTALL_DIR:-"${install_prefix}/esgf_search"}
-seach_data_dir=${ESGF_DATA_DIR:="${esg_root_dir}/search_data"}
-esgf_crawl_launcher="esgf-crawl"
-esgf_index_optimization_launcher="esgf-optimize-index"
-
-if [ ! -e ${esg_functions_file} ]; then
-    checked_get ${esg_functions_file} ${esg_dist_url}$( ((devel == 1)) && echo "/devel" || echo "")/esgf-installer/${esg_functions_file##*/} $((force_install))
-    [ ! -s "${esg_functions_file}" ] && rm ${esg_functions_file}
-fi
-[ -e ${esg_functions_file} ] && source ${esg_functions_file} && ((VERBOSE)) && printf "sourcing from: ${esg_functions_file} \n"
-
-if [ -e /etc/esg.install_log ] && [ ! -e "${install_manifest}" ]; then
-    echo "migrating install manifest to new location"
-    mv -v /etc/esg.install_log  ${install_manifest}
-fi
-
-#---------------------------------------------------------
-# The "main" method for this script
-#---------------------------------------------------------
-#arg (1) - install = 0 [default]
-#          upgrade = 1
-# The other args are a list of <hostname>:<port> of all the replica indexes you wish to create
-setup_search() {
-    echo
-    echo "*******************************"
-    echo "Setting up The ESGF Search Sub-Project..."
-    echo "*******************************"
-    echo
-
-
-
-    write_as_property index_service_endpoint "http://${esgf_host:-$(hostname --fqdn)}/esg-search/search"
-    write_as_property index_service_app_home ${search_web_service_dir}
-    write_as_property index_master_port
-    write_as_property index_slave_port
-    write_as_property index_timeout_connection 2000
-    write_as_property index_timeout_read_datasets 10000
-    write_as_property index_timeout_read_files 60000
-
-    write_as_property publishing_service_endpoint "https://${esgf_host:-$(hostname --fqdn)}/esg-search/remote/secure/client-cert/hessian/publishingService"
-    write_as_property publishing_service_app_home ${search_web_service_dir}
-
-    write_search_rss_properties
-
-    setup_publisher_resources
-    [ $? != 0 ] && echo " WARNING: Could not setup publisher resources" || echo " [OK] "
-    write_as_property esgf_publisher_resources_home
-    write_as_property esgf_publisher_resources_repo
-
-    #Get utility script for crawling thredds sites
-    fetch_crawl_launcher
-    fetch_index_optimization_launcher
-
-    echo "done"
-    echo
-    return 0
-}
 
 def write_search_service_install_log(search_web_service_dir, esg_search_version):
     esg_functions.write_to_install_manifest("webapp:esg-search", search_web_service_dir, esg_search_version)
@@ -142,36 +53,22 @@ def search_startup_hook():
 #--------------------
 # Lifecycle functions
 #--------------------
+def start_search_services():
+    print "Starting search services..."
+    config_facets_props_path = "{}/facets.properties".format(config["esg_config_dir"])
+    esg_search_facets_props_path = "{}/webapps/esg-search/WEB-INF/classes/esg/search/config/facets.properties".format(config["tomcat_install_dir"])
+    if not os.path.exists(config_facets_props_path) and os.path.exists(esg_search_facets_props_path):
+        shutil.copyfile(esg_search_facets_props_path, config_facets_props_path)
 
-start_search_services() {
-    echo "Starting search services... $@"
-    if [ ! -e "${esg_config_dir}/facets.properties" ] && [ -e "${tomcat_install_dir}/webapps/esg-search/WEB-INF/classes/esg/search/config/facets.properties" ]; then
-        cp -v ${tomcat_install_dir}/webapps/esg-search/WEB-INF/classes/esg/search/config/facets.properties ${esg_config_dir}/facets.properties
-    fi
-    #start_zookeeper
-    start_solr $(_load_shards_config)
-}
+    solr.start_solr()
 
-stop_search_services() {
-    echo "Stopping search services..."
-    stop_solr $(_load_shards_config)
-    #stop_zookeeper
-}
-
-
+def stop_search_services():
+    print "Stopping search services..."
+    solr.stop_solr()
 
 #---------------------------------------------------------
 # Solr Search Service Setup and Configuration
 #---------------------------------------------------------
-
-tomcat_install_dir=${tomcat_install_dir:-${install_prefix}/tomcat}
-web_app_tld=${tomcat_install_dir}/webapps
-
-search_web_service_name=esg-search
-search_web_service_dir=${web_app_tld}/${search_web_service_name}
-search_service_dist_url=${esg_dist_url}/${search_web_service_name}/${search_web_service_name}-${esg_search_version}.tar.gz
-
-
 
 def setup_search_service():
     '''
@@ -184,7 +81,7 @@ def setup_search_service():
 
     esg_search_version = "4.9.2"
     print "Checking for search service {}".format(esg_search_version)
-    installed_esg_search_version = esg_property_manager.get_current_webapp_version("esg-search")
+    installed_esg_search_version = esg_version_manager.get_current_webapp_version("esg-search")
 
     if os.path.isdir("/usr/local/tomcat/webapps/esg-search"):
         if esg_property_manager.get_property("install.esg.search"):
@@ -213,7 +110,7 @@ def setup_search_service():
         esg_functions.download_update(search_service_dist_file, search_service_dist_url)
 
         #Extract in workdir to get esg-search.war file
-        esg_functions.extract(search_service_dist_file)
+        esg_functions.extract_tarball(search_service_dist_file)
         search_service_dist_dir = "esg-search-{}".format(esg_search_version)
         with esg_bash2py.pushd(search_service_dist_dir):
             esg_tomcat_manager.stop_tomcat()
@@ -225,7 +122,7 @@ def setup_search_service():
 
             with esg_bash2py.pushd(search_web_service_dir):
                 print "Expanding war {search_service_war_file} in {pwd}".format(search_service_war_file=search_service_war_file, pwd=os.getcwd())
-                esg_functions.extract(search_service_war_file)
+                esg_functions.extract_tarball(search_service_war_file)
 
     print "Checking for Solr schema update"
     new_solr_xml = "{}/WEB-INF/solr-home/mycore/conf/schema.xml".format(search_web_service_dir)
@@ -247,6 +144,15 @@ def setup_search_service():
     TOMCAT_GROUP_ID = esg_functions.get_group_id("tomcat")
 
     esg_functions.change_ownership_recursive(search_web_service_dir, TOMCAT_USER_ID, TOMCAT_GROUP_ID)
+
+    write_search_service_install_log(search_web_service_dir, esg_search_version)
+    write_search_rss_properties()
+    setup_publisher_resources()
+
+    #Get utility script for crawling thredds sites
+    fetch_crawl_launcher()
+    fetch_index_optimization_launcher()
+
 
 
 
@@ -317,7 +223,13 @@ def setup_esg_search():
     esg_functions.change_ownership_recursive("/usr/local/tomcat/webapps/esg-search", TOMCAT_USER_ID, TOMCAT_GROUP_ID)
 
 def main():
-    setup_esg_search()
+    # setup_esg_search()
+    print "*******************************"
+    print "Setting up The ESGF Search Sub-Project..."
+    print "*******************************"
+
+    setup_search_service()
+
 
 if __name__ == '__main__':
     main()

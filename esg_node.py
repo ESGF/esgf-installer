cd46fd7feb8287807e967bc01ed688b95f460404
William Hill <hill119@llnl.gov>
2018-03-16 12:27:48 -0700

    add back setup_security()

--- esg_node.py
+++ esg_node.py
@@ -332,7 +332,7 @@ def system_component_installation(esg_dist_url, node_type_list):
         print "******************************* \n"
         from idp_node import idp, esg_security, globus
         idp.main(esg_dist_url)
-        # esg_security.setup_security(node_type_list, esg_dist_url)
+        esg_security.setup_security(node_type_list, esg_dist_url)
         globus.setup_globus("IDP")
         idp.setup_slcs()
 

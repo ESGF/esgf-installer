e325f186474be311b4dac097906248445a32cc90
William Hill <hill119@llnl.gov>
2018-03-21 12:24:03 -0700

    added setup_temp_ca() to main()

--- esgf_utilities/esg_cert_manager.py
+++ esgf_utilities/esg_cert_manager.py
@@ -747,8 +747,6 @@ def new_ca():
             OpenSSL.crypto.dump_certificate(OpenSSL.crypto.FILETYPE_PEM, cacert).decode('utf-8')
     )
 
-
-
 def newreq_nodes():
     '''Mimics perl CA.pl -newreq-nodes'''
 
@@ -873,7 +871,6 @@ def setup_temp_ca(temp_ca_dir="/etc/tempcerts"):
         # rm -rf $tgtdir;
         shutil.rmtree(globus_cert_dir)
 
-
         # mkdir -p /etc/certs
         esg_bash2py.mkdir_p("/etc/certs")
     	# cp openssl.cnf /etc/certs/
@@ -886,53 +883,6 @@ def setup_temp_ca(temp_ca_dir="/etc/tempcerts"):
         shutil.copyfile("cacert.pem", "/etc/certs/cachain.pem")
     	# mkdir -p /etc/esgfcerts
         esg_bash2py.mkdir_p("/etc/esgfcerts")
-
-    # [ -z "${esgf_host}" ] && get_property esgf_host
-	# hostname=$esgf_host;
-	# mkdir -p /etc/tempcerts;
-	# cd /etc/tempcerts && rm -rf CA; rm -f *.pem; rm -f *.gz; rm -f *.ans; rm -f *.tmpl
-	# mkdir CA
-	# write_ca_ans_templ >setupca.ans.tmpl
-	# write_reqhost.ans_templ >reqhost.ans.tmpl
-	# echo -e "y\ny" >setuphost.ans
-	# cat setupca.ans.tmpl|sed  "s/placeholder.fqdn/$hostname/" >setupca.ans
-	# cat reqhost.ans.tmpl|sed  "s/placeholder.fqdn/$hostname/" >reqhost.ans
-    # curl -s -L --insecure ${esg_dist_url_root}$( ((devel == 1)) && echo "/devel" || echo "")/esgf-installer/CA.pl >CA.pl
-    # curl -s -L --insecure ${esg_dist_url_root}$( ((devel == 1)) && echo "/devel" || echo "")/esgf-installer/openssl.cnf >openssl.cnf
-    # curl -s -L --insecure ${esg_dist_url_root}$( ((devel == 1)) && echo "/devel" || echo "")/esgf-installer/myproxy-server.config >myproxy-server.config
-	# perl CA.pl -newca <setupca.ans
-	# openssl rsa -in CA/private/cakey.pem -out clearkey.pem -passin pass:placeholderpass && mv clearkey.pem CA/private/cakey.pem
-	# perl CA.pl -newreq-nodes <reqhost.ans
-	# perl CA.pl -sign <setuphost.ans
-	# openssl x509 -in CA/cacert.pem -inform pem -outform pem >cacert.pem
-	# cp CA/private/cakey.pem cakey.pem
-	# openssl x509 -in newcert.pem -inform pem -outform pem >hostcert.pem
-	# mv newkey.pem hostkey.pem
-	# chmod 400 cakey.pem
-	# chmod 400 hostkey.pem
-	# rm -f new*.pem
-	# ESGF_OPENSSL=/usr/bin/openssl
-	# cert=cacert.pem
-	# tmpsubj='/O=ESGF/OU=ESGF.ORG/CN=placeholder'
-	# quotedtmpsubj=`echo "$tmpsubj" | sed 's/[./*?|]/\\\\&/g'`;
-	# certsubj=`openssl x509 -in $cert -noout -subject|cut -d ' ' -f2-`;
-	# quotedcertsubj=`echo "$certsubj" | sed 's/[./*?|]/\\\\&/g'`;
-	# echo "quotedcertsubj=~$quotedcertsubj~";
-	# localhash=`$ESGF_OPENSSL x509 -in $cert -noout -hash`;
-	# tgtdir="globus_simple_ca_${localhash}_setup-0";
-	# mkdir $tgtdir;
-	# cp $cert $tgtdir/${localhash}.0;
-	# print_templ >signing_policy_template;
-	# sed "s/\(.*\)$quotedtmpsubj\(.*\)/\1$quotedcertsubj\2/" signing_policy_template >$tgtdir/${localhash}.signing_policy;
-	# cp $tgtdir/${localhash}.signing_policy signing-policy
-	# tar -cvzf globus_simple_ca_${localhash}_setup-0.tar.gz $tgtdir;
-	# rm -rf $tgtdir;
-	# rm -f signing_policy_template;
-	# mkdir -p /etc/certs
-	# cp openssl.cnf /etc/certs/
-	# cp host*.pem /etc/certs/
-	# cp cacert.pem /etc/certs/cachain.pem
-	# mkdir -p /etc/esgfcerts
 
 
 def set_commercial_ca_paths():
@@ -1011,6 +961,7 @@ def main():
     print "******************************* \n"
 
     create_self_signed_cert("/etc/certs")
+    setup_temp_ca()
     install_tomcat_keypair("/etc/certs/hostkey.pem", "/etc/certs/hostcert.pem")
     check_for_commercial_ca()
 

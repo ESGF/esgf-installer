7d04a4607ae16573e49d1aff627833bf847a5ed5
William Hill <hill119@llnl.gov>
2018-03-29 14:39:25 -0700

    defined show_summary(); code clean up

--- esg_node.py
+++ esg_node.py
@@ -6,6 +6,7 @@ import socket
 import platform
 import glob
 import shutil
+import ConfigParser
 import stat
 import yaml
 import semver
@@ -33,27 +34,18 @@ logger = logging.getLogger("esgf_logger" +"."+ __name__)
 with open(os.path.join(os.path.dirname(__file__), 'esg_config.yaml'), 'r') as config_file:
     config = yaml.load(config_file)
 
-os.environ['LANG'] = "POSIX"
-os.umask(022)
-
 node_types = {"INSTALL": False, "DATA": False, "INDEX": False,
                           "IDP": False, "COMPUTE": False, "MIN": 4, "MAX": 64}
 node_types["ALL"] = node_types["DATA"] and node_types[
     "INDEX"] and node_types["IDP"] and node_types["COMPUTE"]
 
 node_type_list = []
+devel = False
 
 def get_node_type():
     for key, value in node_types.items():
         if value:
             node_type_list.append(key)
-
-devel = True
-recommended_setup = 1
-
-custom_setup = 0
-use_local_files = 0
-
 
 def set_version_info():
     '''Gathers the version info from the latest git tag'''
@@ -66,7 +58,6 @@ def set_version_info():
 
     return version, maj_version, release
 
-progname = "esg-node"
 script_version, script_maj_version, script_release = set_version_info()
 force_install = False
 
@@ -260,39 +251,38 @@ def install_log_info():
     if node_types["COMPUTE"]:
         logger.info("(compute node type selected)")
 
-#TODO: implement
 def show_summary():
-    #####
-    # Show user summary and environment variables that have been set
-    #####
-    # show_summary() {
-    #     if [ $((show_summary_latch == 0)) = 1 ]; then return 0; fi
-    #     echo
-    #     echo "-------------------"
-    #     echo "  esgf node run summary: "
-    #     echo "-------------------"
-    #     echo "The following environment variables were used during last full install"
-    #     echo "They are written to the file ${envfile}"
-    #     echo "Please source this file when using these tools"
-    #     echo
-    #     cat ${envfile}
-    #     echo "-------------------"
-    #     echo "Installation Log:"
-    #     echo
-    #     cat ${install_manifest}
-    #     echo "-------------------"
-    #     echo
-    #     return 0
-    # }
-    pass
+    '''Show user summary and environment variables that have been set'''
+    print "-------------------"
+    print " esgf_node Run Summary: "
+    print "-------------------"
+
+    print "The following environment variables were used during last full install"
+    print "They are written to the file {}".format(config["envfile"])
+    print "Please source this file when using these tools"
+
+    try:
+        with open(config["envfile"], 'r') as env_file:
+            print env_file.read()
+    except IOError, error:
+        logger.exception(error)
+
+    print "-------------------"
+    print "Installation Log:"
+    try:
+        with open(config["install_manifest"], 'r') as env_file:
+            print env_file.read()
+    except IOError, error:
+        logger.exception(error)
+    print "-------------------"
+
 
 
 def system_component_installation(esg_dist_url, node_type_list):
-    #---------------------------------------
-    # Installation of basic system components.
-    # (Only when one setup in the sequence is okay can we move to the next)
-    #---------------------------------------
-
+    '''
+    Installation of basic system components.
+    (Only when one setup in the sequence is okay can we move to the next)
+    '''
     if "INSTALL" in node_type_list:
         #TODO: check status of base components; if all running, skip setup
         esg_setup.setup_java()
@@ -345,6 +335,7 @@ def done_remark():
     '''Prints info to denote that the installation has completed'''
     print "\nFinished!..."
     print "In order to see if this node has been installed properly you may direct your browser to:"
+
     if "DATA" in node_type_list or "INSTALL" in node_type_list:
         esgf_host = esg_functions.get_esgf_host()
         print "http://{esgf_host}/thredds".format(esgf_host=esgf_host)
@@ -379,19 +370,16 @@ def setup_esgf_rpm_repo(esg_dist_url):
     print "Setting up ESGF RPM repository"
     print "******************************* \n"
 
-    with open("/etc/yum.repos.d/esgf.repo", "w") as esgf_repo:
-        esgf_repo.write('[esgf]\n')
-        esgf_repo.write('name=ESGF\n')
-        os_version = platform.platform()
-        if "centos" in os_version:
-            esgf_repo.write("baseurl={esg_dist_url}/RPM/centos/6/x86_64\n".format(esg_dist_url=esg_dist_url))
-        if "redhat" in os_version:
-            esgf_repo.write("baseurl={esg_dist_url}/RPM/redhat/6/x86_64\n".format(esg_dist_url=esg_dist_url))
-        esgf_repo.write('failovermethod=priority\n')
-        esgf_repo.write('enabled=1\n')
-        esgf_repo.write('priority=90\n')
-        esgf_repo.write('gpgcheck=0\n')
-        esgf_repo.write('proxy=_none_\n')
+    parser = ConfigParser.SafeConfigParser()
+    parser.read("esgf_utilities/esgf.repo")
+    os_version = platform.platform()
+    if "centos" in os_version:
+        parser.set("esgf", "baseurl", "{esg_dist_url}/RPM/centos/6/x86_64\n".format(esg_dist_url=esg_dist_url))
+    elif "redhat" in os_version:
+        parser.set("esgf", "baseurl", "{esg_dist_url}/RPM/redhat/6/x86_64\n".format(esg_dist_url=esg_dist_url))
+
+    with open("/etc/yum.repos.d/esgf.repo", "w") as repo_file_object:
+        parser.write(repo_file_object)
 
 
 def main(node_type_list):
@@ -421,17 +409,11 @@ def main(node_type_list):
 
     logger.debug("node_type_list: %s", node_type_list)
     logger.info("node_type_list: %s", node_type_list)
-    print "node_type_list after process_arguments:", node_type_list
 
     print '''
     -----------------------------------
     ESGF Node Installation Program
     -----------------------------------'''
-
-    # #If not type not set from CLI argument, look at previous node type setting
-    # if not [node_type for node_type in node_type_list if node_type in node_types.keys()]:
-    #     previous_node_type = esg_cli_argument_manager.get_node_type()
-    #     print "previous_node_type:", previous_node_type
     check_selected_node_type(node_types, node_type_list)
 
     # Display node information to user
@@ -447,7 +429,7 @@ def main(node_type_list):
 
     esg_questionnaire.initial_setup_questionnaire()
 
-    # setup_esgf_rpm_repo(esg_dist_url)
+    setup_esgf_rpm_repo(esg_dist_url)
 
     # install dependencies
     system_component_installation(esg_dist_url, node_type_list)

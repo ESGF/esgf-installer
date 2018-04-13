9a0135a56598b07ff2c53e8ec5311b045d464db6
William Hill <hill119@llnl.gov>
2018-04-13 14:26:05 -0700

    minor fixes to test; removed dead code; resolves #404

--- tests/test_esg_publisher.py
+++ tests/test_esg_publisher.py
@@ -2,6 +2,7 @@ import unittest
 import os
 import shutil
 import fnmatch
+import logging
 import re
 from context import esgf_utilities
 from context import base
@@ -11,12 +12,15 @@ from data_node import thredds
 from base import esg_postgres
 from esgf_utilities import esg_bash2py
 from esgf_utilities import esg_functions
+from esgf_utilities.esg_exceptions import SubprocessError
 import yaml
 
 current_directory = os.path.join(os.path.dirname(__file__))
 
 with open(os.path.join(current_directory, os.pardir, 'esg_config.yaml'), 'r') as config_file:
     config = yaml.load(config_file)
+
+logger = logging.getLogger("esgf_logger" + "." + __name__)
 
 class test_ESG_publisher(unittest.TestCase):
 
@@ -91,9 +95,21 @@ class test_ESG_publisher(unittest.TestCase):
         print "Publication Test"
         print "******************************* \n"
 
-        esg_functions.call_subprocess("groupadd tomcat")
-        esg_functions.call_subprocess("useradd -s /sbin/nologin -g tomcat -d /usr/local/tomcat tomcat")
-        thredds.setup_thredds()
+        try:
+            esg_functions.call_subprocess("groupadd tomcat")
+        except SubprocessError, error:
+            logger.debug(error[0]["returncode"])
+            if error[0]["returncode"] == 9:
+                pass
+        try:
+            esg_functions.call_subprocess("useradd -s /sbin/nologin -g tomcat -d /usr/local/tomcat tomcat")
+        except SubprocessError, error:
+            logger.debug(error[0]["returncode"])
+            if error[0]["returncode"] == 9:
+                pass
+
+        if not os.path.isdir("/usr/local/tomcat/webapps/thredds"):
+            thredds.setup_thredds()
 
         if not esg_postgres.postgres_status():
             esg_postgres.start_postgres()
@@ -120,75 +136,6 @@ class test_ESG_publisher(unittest.TestCase):
 
         esg_functions.stream_subprocess_output("esgtest_publish")
 
-
-
-
-
-    # def test_checkout_publisher_branch(self):
-    #     repo = checkout_publisher_branch("/tmp/esg-publisher", "v3.2.7")
-    #     branch = repo.active_branch
-    #     print "active branch:", branch.name
-    #     self.assertEquals(branch.name, "v3.2.7")
-
-    # def test_run_esgsetup(self):
-    #     esg_publisher.run_esgsetup()
-    #     self.assertTrue(os.path.isfile("/esg/config/esgcet/esg.ini"))
-
-
-
-    # def test_esgcet(self):
-    #     print "\n*******************************"
-    #     print "Publisher Test"
-    #     print "******************************* \n"
-    #     starting_directory = os.getcwd()
-    #     os.chdir(config["workdir"])
-    #
-    #     esg_postgres.start_postgres()
-    #
-    #     esgcet_testdir = os.path.join(config[
-    #                                   "thredds_root_dir"], "test")
-    #     esg_bash2py.mkdir_p(esgcet_testdir)
-    #
-    #     os.chown(esgcet_testdir, config[
-    #              "installer_uid"], config["installer_gid"])
-    #
-    #     esg_bash2py.mkdir_p(config["thredds_replica_dir"])
-    #
-    #     os.chown(config["thredds_replica_dir"], config[
-    #              "installer_uid"], config["installer_gid"])
-    #     print "esgcet test directory: [%s]" % esgcet_testdir
-    #
-    #     fetch_file = "sftlf.nc"
-    #     if not esg_functions.download_update(os.path.join(esgcet_testdir, fetch_file), "http://" + config["esg_dist_url_root"] + "/externals/" + fetch_file):
-    #         print " ERROR: Problem pulling down %s from esg distribution" % (fetch_file)
-    #         esg_functions.exit_with_error(1)
-    #
-    #     # Run test...
-    #     print "%s/bin/esginitialize -c " % (config["cdat_home"])
-    #     esginitialize_output = subprocess.call(
-    #         "%s/bin/esginitialize -c" % (config["cdat_home"]), shell=True)
-    #
-    #     print '''
-    #         {cdat_home}/bin/esgprep mapfile --dataset ipsl.fr.test.mytest --project test {esgcet_testdir}; mv ipsl.fr.test.mytest.map test_mapfile.txt
-    #         '''.format(cdat_home=config["cdat_home"], esg_org_name=esg_org_name, node_short_name=node_short_name, esgcet_testdir=esgcet_testdir)
-    #     esgprep_output = subprocess.call('''
-    #         {cdat_home}/bin/esgprep mapfile --dataset ipsl.fr.test.mytest --project test {esgcet_testdir}; mv ipsl.fr.test.mytest.map test_mapfile.txt
-    #         '''.format(cdat_home=config["cdat_home"], esg_org_name=esg_org_name, node_short_name=node_short_name, esgcet_testdir=esgcet_testdir), shell=True)
-    #     if esgprep_output != 0:
-    #         print " ERROR: ESG Mapfile generation failed"
-    #         os.chdir(starting_directory)
-    #         esg_functions.exit_with_error(1)
-    #
-    #     print "{cdat_home}/bin/esgpublish --service fileservice --map test_mapfile.txt --project test --thredds".format(cdat_home=config["cdat_home"])
-    #     esgpublish_output = subprocess.call("{cdat_home}/bin/esgpublish --service fileservice --map test_mapfile.txt --project test --thredds".format(
-    #         cdat_home=config["cdat_home"]), shell=True)
-    #     if esgpublish_output != 0:
-    #         print " ERROR: ESG publish failed"
-    #         os.chdir(starting_directory)
-    #         esg_functions.exit_with_error(1)
-    #
-    #     os.chdir(starting_directory)
-    #     esg_functions.exit_with_error(0)
 
 if __name__ == '__main__':
     unittest.main()

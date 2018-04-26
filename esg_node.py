ef137e005c159189b817c8e2b43140331dc83433
William Hill <hill119@llnl.gov>
2018-04-26 12:01:55 -0700

    specified version for esgprep

--- esg_node.py
+++ esg_node.py
@@ -220,7 +220,7 @@ def system_component_installation(esg_dist_url, node_type_list):
         print "\n*******************************"
         print "Installing Data Node Components"
         print "******************************* \n"
-        pip.main(['install', "esgprep"])
+        pip.main(['install', "esgprep==2.7.38"])
         esg_publisher.main()
         from data_node import esg_dashboard, orp, thredds
         from idp_node import globus

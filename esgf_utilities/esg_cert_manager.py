8ada2099234fa14c3d398f79090c562c7d301804
William Hill <hill119@llnl.gov>
2018-06-29 15:33:33 -0700

    removed logging and comments

--- esgf_utilities/esg_cert_manager.py
+++ esgf_utilities/esg_cert_manager.py
@@ -685,18 +685,10 @@ def createCertRequest(pkey, digest="sha256", **name):
     """
     req = OpenSSL.crypto.X509Req()
     subj = req.get_subject()
-    logger.debug("subj: %s", subj)
 
-    logger.debug("name: %s", name)
-    logger.debug("name type: %s", type(name))
-    logger.debug("name[O]: %s", name["O"])
     setattr(subj, "O", name["O"])
     setattr(subj, "OU", name["OU"])
     setattr(subj, "CN", name["CN"])
-    # for key, value in name.items():
-    #     logger.debug("key: %s", key)
-    #     logger.debug("value: %s", value)
-    #     setattr(subj, key, value)
 
     req.set_pubkey(pkey)
     req.sign(pkey, digest)
@@ -719,7 +711,6 @@ def createCertificate(req, issuerCertKey, serial, validityPeriod,
     Returns:   The signed certificate in an X509 object
     """
     issuerCert, issuerKey = issuerCertKey
-    logger.debug("issuerCert: %s", issuerCert)
     notBefore, notAfter = validityPeriod
     cert = OpenSSL.crypto.X509()
     cert.set_serial_number(serial)
@@ -960,7 +951,6 @@ def generate_ssl_key_and_csr(private_key="/usr/local/tomcat/hostkey.pem", public
         public_cert_req = "/usr/local/tomcat/{}-esg-node.csr".format(esg_functions.get_esgf_host())
 
     public_cert_dn = extract_keystore_dn()
-    logger.debug("public_cert_dn: %s", public_cert_dn)
     if public_cert_dn:
         careq = createCertRequest(private_key, public_cert_dn)
     else:

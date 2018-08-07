9ac22dd6746964018ac0dd3fc4f4a826c2722cfe
William Hill <hill119@llnl.gov>
2018-08-07 10:39:31 -0700

    removed pip uninstall step

--- tests/test_esg_apache.py
+++ tests/test_esg_apache.py
@@ -21,7 +21,6 @@ class test_ESG_apache(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         esg_functions.stream_subprocess_output("yum remove -y httpd")
-        esg_functions.stream_subprocess_output("pip uninstall -y mod_wsgi")
         try:
             shutil.rmtree("/var/www/.python-eggs")
             shutil.rmtree('/var/www/html/')

abd64acf77b68e5c7ed0e6af9c6798177b2f6fb8
William Hill <hill119@llnl.gov>
2018-08-09 16:05:29 -0700

    added manifest_file argument

--- tests/test_esg_functions.py
+++ tests/test_esg_functions.py
@@ -67,9 +67,9 @@ class test_ESG_Functions(unittest.TestCase):
     def test_write_to_install_manifest(self):
         esg_functions.write_to_install_manifest("foo_app", "/tmp/foo", "1.0", "/tmp/install_manifest")
         self.assertTrue(os.path.isfile("/tmp/install_manifest"))
-        prop = esg_functions.get_version_from_install_manifest("foo_app")
+        prop = esg_functions.get_version_from_install_manifest("foo_app", manifest_file="/tmp/install_manifest")
         logger.info("prop: %s", prop)
-        self.assertEqual("1.0", prop)
+        self.assertTrue("1.0" in prop)
 
     def test_is_valid_password(self):
         self.assertTrue(esg_functions.is_valid_password("Esgf4Dev"))

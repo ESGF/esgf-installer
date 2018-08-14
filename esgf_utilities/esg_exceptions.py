7ae9b5a672ad865b35beb5fa64aeacf14fa1c66b
William Hill <hill119@llnl.gov>
2018-08-14 09:02:26 -0700

    fixed __str__

--- esgf_utilities/esg_exceptions.py
+++ esgf_utilities/esg_exceptions.py
@@ -17,6 +17,6 @@ class SubprocessError(Exception):
     def __init__(self, data):
         self.data = data
     def __str__(self):
-        return dict(self.data)
+        return repr(self.data)
     def __repr__(self):
         return dict(self.data)

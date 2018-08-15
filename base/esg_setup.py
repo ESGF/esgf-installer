6041bfeb3351f07463a11cf6a1aaee2ffda86308
Carlson <carlson60@graduation.llnl.gov>
2018-08-15 11:19:36 -0700

    Remove unused functions and reformat prerequesite checks
    The removed functionality has been replaced by the esgf_functions
    module. The prerequesite checks use assertions and AssertionError
    to determine if the requirements are satisfied.

--- base/esg_setup.py
+++ base/esg_setup.py
@@ -5,10 +5,8 @@ import logging
 import platform
 import netifaces
 import yaml
-from esgf_utilities.esg_exceptions import WrongOSError
 from esgf_utilities import pybash
 from esgf_utilities import esg_functions
-from esgf_utilities import esg_property_manager
 
 logger = logging.getLogger("esgf_logger" +"."+ __name__)
 
@@ -16,56 +14,68 @@ with open(os.path.join(os.path.dirname(__file__), os.pardir, 'esg_config.yaml'),
     config = yaml.load(config_file)
 
 def check_if_root():
-    '''Check to see if the user is root'''
-    print "Checking that you have root privileges on %s... " % (socket.gethostname())
-    root_check = os.geteuid()
-    try:
-        if root_check != 0:
-            raise UnprivilegedUserError
-        logger.debug("Root user found.")
-        return True
-    except UnprivilegedUserError:
-        logger.exception("\nMust run this program with root's effective UID\n\n")
-        esg_functions.exit_with_error(1)
+    '''Check to see if the user has root privileges'''
+
+    print "Checking for root privileges on {host}...".format(host=socket.gethostname())
+
+    err_msg = "\nThis program must be run with root privileges\n\n"
+    assert (os.geteuid() == 0), err_msg
+
+    logger.debug("Root privileges found")
 
 def check_os():
-    '''Check if the operating system on server is Redhat or CentOS;
-    returns False Otherwise'''
-    print "Checking operating system....."
+    ''' Check if the operating system on server is Redhat or CentOS '''
+
+    print "Checking operating system..."
+
     release_version = re.search(
-        "(centos|redhat)-(\S*)-", platform.platform()).groups()
+        r"(centos|redhat)-(\S*)-",
+        platform.platform()
+    ).groups()
+
     logger.debug("Release Version: %s", release_version)
-    try:
-        if "6" not in release_version[1]:
-            raise WrongOSError
-    except WrongOSError:
-        logger.exception("ESGF can only be installed on versions 6 of Red Hat, CentOS or Scientific Linux x86_64 systems")
-        esg_functions.exit_with_error(1)
-    else:
-        print "Operating System = {OS} {version}".format(OS=release_version[0], version=release_version[1])
-        return True
+
+    err_msg = """
+        ESGF can only be installed on versions 6 of Red Hat,
+        CentOS or Scientific Linux x86_64 systems
+    """
+    assert ("6" in release_version[1]), err_msg
+
+    print "Operating System = {OS} {version}".format(
+        OS=release_version[0],
+        version=release_version[1]
+    )
 
 
 def check_prerequisites():
     '''
-        Checking for what we expect to be on the system a-priori that we are not going to install or be responsible for
+        A check for what is expected to be on the system a-priori that we are
+        not going to install or be responsible for.
     '''
 
-    if not check_if_root():
-        return False
-
-    #----------------------------------------
-    print "Checking requisites... "
+    print "Checking prerequisites..."
+    checks = [check_if_root, check_os]
+    for check in checks:
+        try:
+            check()
+        except AssertionError as err:
+            logger.exception(str(err))
+            esg_functions.exit_with_error(1)
 
     # checking for OS, architecture, distribution and version
-    return check_os()
+    return True
 
 def create_esg_directories():
     '''Create directories to hold ESGF scripts, config files, and logs'''
-    directories_to_check = [config["scripts_dir"], config["esg_backup_dir"], config["esg_tools_dir"],
-                            config[
-                                "esg_log_dir"], config["esg_config_dir"], config["esg_etc_dir"],
-                            config["tomcat_conf_dir"]]
+    directories_to_check = [
+        config["scripts_dir"],
+        config["esg_backup_dir"],
+        config["esg_tools_dir"],
+        config["esg_log_dir"],
+        config["esg_config_dir"],
+        config["esg_etc_dir"],
+        config["tomcat_conf_dir"]
+    ]
     for directory in directories_to_check:
         if not os.path.isdir(directory):
             pybash.mkdir_p(directory)
@@ -84,61 +94,3 @@ def init_structure():
     #--------------
 
     esg_functions.get_public_ip()
-
-def _select_ip_address():
-    choice = int(raw_input(""))
-    return choice
-
-
-def _render_ip_address_menu(ip_addresses):
-    print "Detected multiple IP addresses bound to this host...\n"
-    print "Please select the IP address to use for this installation\n"
-    print "\t-------------------------------------------\n"
-    for index, ip_address in enumerate(ip_addresses.iteritems(), 1):
-        print "\t %i) %s" % (index, ip_address)
-    print "\t-------------------------------------------\n"
-
-
-def check_for_my_ip(force_install=False):
-    '''Check server's IP address'''
-    logger.debug("Checking for IP address(es)...")
-    matched = 0
-    my_ip_address = None
-    eth0 = netifaces.ifaddresses(netifaces.interfaces()[1])
-    ip_addresses = [ip["addr"] for ip in eth0[netifaces.AF_INET]]
-
-    try:
-        esgf_host_ip
-    except NameError:
-        esgf_host_ip = esg_property_manager.get_property("esgf.host.ip")
-
-    if esgf_host_ip and not force_install:
-        logger.info("Using IP: %s", esgf_host_ip)
-        return 0
-
-    # We want to make sure that the IP address we have in our config
-    # matches one of the IPs that are associated with this host
-    for ip_address in ip_addresses:
-        if ip_address == esgf_host_ip:
-            matched += 1
-
-    if matched == 0:
-        logger.info(
-            "Configured host IP address does not match available IPs...")
-
-    if not esgf_host_ip or force_install or matched == 0:
-        if len(ip_addresses) > 1:
-            # ask the user to choose...
-            while True:
-                _render_ip_address_menu(ip_addresses)
-                default = 0
-                choice = _select_ip_address() or default
-                my_ip_address = ip_addresses[choice]
-                logger.info("selected address -> %s", my_ip_address)
-                break
-        else:
-            my_ip_address = ip_addresses[0]
-
-    esg_property_manager.set_property("esgf_host_ip", my_ip_address)
-    esgf_host_ip = esg_property_manager.get_property("esgf.host.ip")
-    return esgf_host_ip

14606184e66bbd34cd500b26e99af24155adda50
Nathan Carlson <nathan.l.carlson@outlook.com>
2018-08-30 13:43:45 -0700

    Zero is not a valid key

--- installer/install_codes.py
+++ installer/install_codes.py
@@ -1,4 +1,4 @@
 '''Codes to be used for communciation between Installer and Components to figure out what to do'''
-OK = 0
-NOT_INSTALLED = 1
-BAD_VERSION = 2
+OK = 1
+NOT_INSTALLED = 2
+BAD_VERSION = 3

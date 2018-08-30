1badfcd845b074c9662a027253940bef3e3ff218
Nathan Carlson <nathan.l.carlson@outlook.com>
2018-08-30 12:32:51 -0700

    Make reading from a INI config file and getting a property disctint
    A property is a value that is relevant to the installer and the
    eventual configuration of the node. They fall into many different
    categories. Some have a finite set of choices, some may be None,
    some may need to match a regex, some require prompting the user,
    others may not, etc. This allows for the ability to obtain values
    while maintaining restrictions on that value as well as where to
    get the value from.

--- esgf_utilities/esg_property_manager.py
+++ esgf_utilities/esg_property_manager.py
@@ -12,37 +12,100 @@ logger = logging.getLogger("esgf_logger" +"."+ __name__)
 with open(os.path.join(os.path.dirname(__file__), os.pardir, 'esg_config.yaml'), 'r') as config_file:
     config = yaml.load(config_file)
 
-def get_property(property_name, property_file=config["property_file"], section_name="installer.properties", separator=".", not_none=True):
-    '''
-        Gets a single property from the property_file using ConfigParser
-        arg 1 - the string that you wish to get the property of (and make a variable)
-        arg 2 - the path to the config file
-    '''
-    property_name = property_name.replace("_", separator)
-    parser = ConfigParser.SafeConfigParser(allow_no_value=True)
-    parser.read(property_file)
+def build_prompt(msg=None, default=None, choices=None, not_none=True):
+
+    if default is None and not_none:
+        prompt_str = "{}: ".format(msg)
+    elif choices is None:
+        prompt_str = "{} [{}]: ".format(msg, default)
+    else:
+        choice_str = "({})".format("|".join(choices))
+        prompt_str = "{} {} [{}]: ".format(msg, choice_str, default)
+
+    return prompt_str
+
+def _validate(value, choices=None, not_none=True):
+    if choices is not None:
+        if value not in choices:
+            print "{} not in choices".format(value)
+            return False
+    if not_none:
+        if value is None:
+            print "Input cannot be None"
+            return False
+    return True
+
+def clean(value):
+
+    if value is not None:
+        return value.strip()
+    return value
+
+def _prompt(key, msg=None, default=None, choices=None, not_none=True):
+
+    prompt_str = build_prompt(msg, default, choices, not_none)
+    # Get input and check if it is valid
+    is_valid = False
+    while not is_valid:
+        new_value = raw_input(prompt_str) or default
+        new_value = clean(new_value)
+        is_valid = _validate(new_value, choices, not_none)
+
+    # Do something with the new value
+    return new_value
+
+def get_yes_no_property(key, prompt=None, default="no"):
+
+    default = default.strip().lower()
+    new_value = get_property(key, prompt, default=default, choices=["y", "n", "yes", "no"])
+    return new_value.lower().startswith('y')
+
+def get_property(key, prompt=None, default=None, choices=None, not_none=True):
+
+    separator = "."
+    section = "installer.properties"
+    key = key.replace("_", separator)
     try:
-        value = parser.get(section_name, property_name)
+        new_value = read_config(key, section, config["property_file"], not_none=not_none)
+        new_value = clean(new_value)
+        if prompt is not None and not _validate(new_value, choices, not_none):
+            new_value = _prompt(key, prompt, default, choices, not_none)
+            set_property(key, new_value)
+    except ConfigParser.NoOptionError:
+        new_value = None
+        if prompt is not None:
+            new_value = _prompt(key, prompt, default, choices, not_none)
+            set_property(key, new_value)
+
+    return new_value
+
+def set_property(key, value, separator="."):
+
+    key = key.replace("_", separator)
+    write_config(key, value, "installer.properties", config["property_file"])
+
+def read_config(key, section, filename, not_none=True):
+
+    parser = ConfigParser.SafeConfigParser(allow_no_value=True)
+    parser.read(filename)
+    try:
+        value = parser.get(section, key)
         if not_none and (value is None or value == ""):
-            raise ConfigParser.NoOptionError(property_name, section_name)
+            raise ConfigParser.NoOptionError(key, section)
+        logger.info("%s = %s", key, value)
         return value
     except ConfigParser.NoSectionError:
-        logger.debug("Could not find section: %s", section_name)
+        logger.debug("Could not find section: %s", section)
         raise
     except ConfigParser.NoOptionError:
-        logger.debug("Could not find property or property has no value: %s", property_name)
+        logger.debug("Could not find property or property has no value: %s", key)
         raise
 
-def set_property(property_name, property_value=None, property_file=config["property_file"], section_name="installer.properties", separator="."):
-    '''
-        Writes variable out to property file using ConfigParser
-        arg 1 - The string of the variable you wish to write as property to property file
-        arg 2 - The value to set the variable to (default: None)
-    '''
-    property_name = property_name.replace("_", separator)
-    parser = ConfigObj(property_file)
-    if section_name not in parser:
-        parser[section_name] = {}
-    parser[section_name][property_name] = property_value
+def write_config(key, value, section, filename):
+
+    parser = ConfigObj(filename)
+    if section not in parser:
+        parser[section] = {}
+    parser[section][key] = value
     parser.write()
-    logger.info("Set property: %s = %s", property_name, property_value)
+    logger.info("Set property: %s = %s", key, value)

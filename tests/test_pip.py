d196611f46a2455d68d2cffe64124136ea1e53a3
Nathan Carlson <nathan.l.carlson@outlook.com>
2018-09-06 13:50:02 -0700

    Add -y to bypass prompts from pip uninstall

--- tests/test_pip.py
+++ tests/test_pip.py
@@ -9,7 +9,6 @@ class test_Pip(unittest.TestCase):
         repo = "https://github.com/bast/somepackage.git"
         version = "1.2.3"
 
-        esg_functions.call_binary("pip", ["uninstall", pkg])
         cur_version = esg_functions.pip_version(pkg)
         self.assertTrue(cur_version is None)
 
@@ -17,7 +16,7 @@ class test_Pip(unittest.TestCase):
         cur_version = esg_functions.pip_version(pkg)
         self.assertTrue(cur_version == version)
 
-        esg_functions.call_binary("pip", ["uninstall", pkg])
+        esg_functions.call_binary("pip", ["uninstall", "-y", pkg])
         cur_version = esg_functions.pip_version(pkg)
         self.assertTrue(cur_version is None)
 
@@ -25,7 +24,7 @@ class test_Pip(unittest.TestCase):
         cur_version = esg_functions.pip_version(pkg)
         self.assertTrue(cur_version == version)
 
-        esg_functions.call_binary("pip", ["uninstall", pkg])
+        esg_functions.call_binary("pip", ["uninstall", "-y", pkg])
         cur_version = esg_functions.pip_version(pkg)
         self.assertTrue(cur_version is None)
 

f12d863f815293384b88b7c71e161cf478f4dcf0
Nathan Carlson <nathan.l.carlson@outlook.com>
2018-09-10 08:58:39 -0700

    Increase flexibility by finding from list of packages

--- installer/methods.py
+++ installer/methods.py
@@ -1,4 +1,6 @@
 from plumbum import local
+from plumbum import TEE
+import os
 
 class Method(object):
     def __init__(self, components):
@@ -7,18 +9,40 @@ class Method(object):
 class PackageManager(Method):
     def __init__(self, components):
         Method.__init__(self, components)
-        self.installer = local["yum"]
-        self.manager = local["rpm"]
+        self.installers = {
+            "yum": {
+                "install_y": ["install", "-y"]
+            },
+            "apt": {
+                "install_y": ["install", "-y"]
+            }
+        }
+        self.managers = {
+            "rpm": {
+                "version": ["-q", "--queryformat", "%{VERSION}"]
+            }
+        }
+        self.installer = local.get(*self.installers.keys())
+        self.installer_name = os.path.basename(str(self.installer))
+        self.manager = local.get(*self.managers.keys())
+        self.manager_name = os.path.basename(str(self.manager))
 
     def install(self):
-        pkg_list = [component.pkg_names[self.installer] for component in self.components]
-        args = ["install", "-y"] + pkg_list
-        self.installer.__getitem__(args)
+        # for component in self.components:
+        #     component.pre_install()
+
+        pkg_list = [component.pkg_names[self.installer_name] for component in self.components]
+        args = self.installers[self.installer_name]["install_y"] + pkg_list
+        result = self.installer.__getitem__(args) & TEE
+
+        # for component in self.components:
+        #     component.post_install()
 
     def versions(self):
-        pass
-        #rpm -q pkg_name --queryformat "%{VERSION}"
+        for component in self.components:
+            args = self.managers[self.manager_name]["version"] + component.pkg_names[self.installer_name]
+            result = self.manager.__getitem__(args) & TEE
 
 class Mirror(Method):
     def __init__(self, components):
-        pass
+        Method.__init__(self, components)

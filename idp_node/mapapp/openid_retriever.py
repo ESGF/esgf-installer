4bfc44619d817a3e5e2344f3ca7f2f55fce68949
Nathan Carlson <nathan.l.carlson@outlook.com>
2018-10-01 08:57:33 -0700

    Create seperate functions for each retrieval
    The Python script must retrieve the subject line from a specified
    cert file and the open id for a specified user. I do not know why
    it is this cert file that is being used, only that it was what has
    been used. Absolute paths are used to ensure the correct file is
    executed, but at the cost of fragility. The installer must install
    these components into the correct directory, /esg/config/myproxy

--- idp_node/mapapp/openid_retriever.py
+++ idp_node/mapapp/openid_retriever.py
@@ -4,7 +4,52 @@ import logging
 import os
 import sys
 
+import OpenSSL
 import psycopg2
+
+def get_openid(args, config_parser, esgf_home):
+
+    pass_file = os.path.join(esgf_home, "config", ".esg_pg_pass")
+    with open(pass_file, "rb") as filep:
+        password = filep.read().strip()
+
+    section = "installer.properties"
+
+    db = config_parser.get(section, "db.database")
+    host = config_parser.get(section, "db.host")
+    port = config_parser.get(section, "db.port")
+    user = config_parser.get(section, "db.user")
+    esgf_host = config_parser.get(section, "esgf.host")
+    gateway = "https://{}/esgf-idp/openid/%".format(esgf_host)
+
+    cs = "dbname=%s user=%s password=%s host=%s port=%s" % (db, user, password, host, port)
+    cs_no_pass = "dbname=%s user=%s host=%s port=%s" % (db, user, host, port)
+    logging.info(cs_no_pass)
+    conn = psycopg2.connect(cs)
+    cur = conn.cursor()
+    query = "SELECT DISTINCT openid FROM esgf_security.user"
+    query += " WHERE username=%(username)s AND openid LIKE %(gateway)s "
+    values = {
+        "username": args.username,
+        "gateway": gateway
+    }
+    logging.info(query, values)
+    cur.execute(query, values)
+    result = cur.fetchone()
+    if not result:
+        output = "No OpenID found for {}".format(args.username)
+        print output
+        logging.error(output)
+        sys.exit(1)
+    logging.info("OpenID Success: %s", result[0])
+    return result[0]
+
+def get_subject_line(args):
+
+    with open(args.cert_file) as cert:
+        cert_contents = cert.read()
+    cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert_contents)
+    return cert.get_subject()
 
 def main():
 
@@ -12,6 +57,7 @@ def main():
         description="A script for querying the esgcet database for an OpenID, given a username"
     )
     parser.add_argument("username",  help="The username for which to query for the OpenID")
+    parser.add_argument("cert_file",  help="The username for which to query for the OpenID")
     args = parser.parse_args()
 
     try:
@@ -23,7 +69,7 @@ def main():
         format="%(asctime)s - %(message)s",
         datefmt='%m/%d/%Y %I:%M:%S %p',
         level=logging.DEBUG,
-        filename=os.path.join(esgf_home, "config", "myproxy", "mapapp.og"),
+        filename=os.path.join(esgf_home, "config", "myproxy", "mapapp.log"),
         maxBytes=2*1024*1024
     )
     properties_file = os.path.join(esgf_home, "config", "esgf.properties")
@@ -31,38 +77,15 @@ def main():
     config_parser = ConfigParser.SafeConfigParser(allow_no_value=True)
     config_parser.read(properties_file)
 
-    section = "installer.properties"
-
-    db = config_parser.get(section, "db.database")
-    host = config_parser.get(section, "db.host")
-    port = config_parser.get(section, "db.port")
-    user = config_parser.get(section, "db.user")
-    esgf_host = config_parser.get(section, "esgf.host")
-    gateway = "https://{}/esgf-idp/openid/%".format(esgf_host)
-
-    pass_file = os.path.join(esgf_home, "config", ".esg_pg_pass")
-    with open(pass_file, "rb") as filep:
-        password = filep.read().strip()
-
-    cs = "dbname=%s user=%s password=%s host=%s port=%s" % (db, user, password, host, port)
-    conn = psycopg2.connect(cs)
-    cur = conn.cursor()
-    query = "SELECT DISTINCT openid FROM esgf_security.user"
-    query += " WHERE username=%(username)s AND openid LIKE %(gateway)s "
-    values = {
-        "username": args.username,
-        "gateway": gateway
-    }
-    cur.execute(query, values)
-    result = cur.fetchone()
-    if not result:
-        output = "No OpenID found for {}".format(args.username)
-        output += "\ndbname=%s user=%s host=%s port=%s\n" % (db, user, host, port)
-        output += query % values
-        print output
-        logging.error(output)
-        sys.exit(1)
-    logging.info("Success %s", result[0])
-    print result[0]
+    openid = get_openid(args, config_parser, esgf_home)
+    subject_line = get_subject_line(args)
+    output = "/O={}/OU={}/CN={}"
+    output = output.format(
+        subject_line.O,
+        subject_line.OU,
+        openid
+    )
+    print output
+    logging.info("Sent to myproxy: %s", output)
 
 main()

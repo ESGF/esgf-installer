b9fea8923b0a197ea51c1e85d4ac62bd10ef5e91
William Hill <hill119@llnl.gov>
2018-10-04 09:15:13 -0400

    added test for setup_java()

--- tests/test_esg_java.py
+++ tests/test_esg_java.py
@@ -8,8 +8,11 @@ from context import esgf_utilities
 from context import base
 from base import esg_java
 from esgf_utilities import esg_functions, esg_property_manager
+from esg_purge import purge_java
 from idp_node import myproxy
 import yaml
+from plumbum.commands import ProcessExecutionError
+from plumbum.commands import CommandNotFound
 
 current_directory = os.path.join(os.path.dirname(__file__))
 
@@ -18,7 +21,20 @@ with open(os.path.join(current_directory, os.pardir, 'esg_config.yaml'), 'r') as
 
 logger = logging.getLogger("esgf_logger" + "." + __name__)
 
+
 class test_esg_java(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        logger.info("Setting up esg_java test harness")
+        if esg_java.check_for_existing_java():
+            purge_java()
+
+    @classmethod
+    def tearDownClass(cls):
+        logger.info("Tearing down esg_java test harness")
+        purge_java()
+
     def test_setup_ant(self):
         esg_java.setup_ant()
         self.assertTrue(os.path.exists("/usr/bin/ant"))
@@ -29,9 +45,9 @@ class test_esg_java(unittest.TestCase):
         logger.debug("output: %s", output)
         self.assertTrue(output)
 
-
-
-
+    def test_setup_java(self):
+        esg_java.setup_java()
+        self.assertTrue(esg_java.check_java_version())
 
 
 if __name__ == '__main__':

167ebe4ad90b7fd43e20c70b4f33e016fe7d7589
Nathan Carlson <nathan.l.carlson@outlook.com>
2018-10-05 16:10:54 -0700

    Values must be strings

--- esgf_utilities/esg_property_manager.py
+++ esgf_utilities/esg_property_manager.py
@@ -44,6 +44,6 @@ def set_property(property_name, property_value=None, property_file=config["prope
     parser.read(property_file)
     if section_name not in parser:
         parser[section_name] = {}
-    parser[section_name][property_name] = property_value
+    parser[section_name][property_name] = str(property_value)
     with open(property_file, "w") as file_object:
         parser.write(file_object, space_around_delimiters=False)

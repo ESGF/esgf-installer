853d4508d348cf38f6c3474ac61af7595acfa946
Nathan Carlson <nathan.l.carlson@outlook.com>
2018-10-09 14:17:56 -0700

    Begin cleaning up unused heavyweight components

--- installer/controllers/service.py
+++ installer/controllers/service.py
@@ -3,20 +3,21 @@ import logging
 from plumbum import local
 from plumbum import TEE
 
-from ..components.generic import GenericComponent
+from ..methods.generic import Generic
 
-class Service(GenericComponent):
+class Service(Generic):
     def __init__(self, name, config):
-        GenericComponent.__init__(self, name, config)
+        Generic.__init__(self, [config])
+        config = self.components[0]
         self.log = logging.getLogger(__name__)
         self.service = local["service"]
         self.start_arg = ["start"]
         self.stop_arg = ["stop"]
         self.restart_arg = ["restart"]
         try:
-            self.service_name = self.config["service_name"]
+            self.service_name = config["service_name"]
         except KeyError:
-            self.service_name = self.name
+            self.service_name = name
 
     def start(self):
         args = [self.service_name] + self.start_arg

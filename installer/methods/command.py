f91ad276d946e9cebb6089991b72142eb15412b9
Nathan Carlson <nathan.l.carlson@outlook.com>
2018-10-17 12:23:33 -0700

    Ensure the conda env exists before executing commands in it

--- installer/methods/command.py
+++ installer/methods/command.py
@@ -3,13 +3,13 @@ import os.path as path
 from plumbum import local
 from plumbum import TEE
 
-from .generic import Generic
+from .conda import Conda
 from ..utils import pushd
 
-class Command(Generic):
+class Command(Conda):
     ''' Install components using and arbibtrary command '''
     def __init__(self, components):
-        Generic.__init__(self, components)
+        Conda.__init__(self, components)
         self.executed = set()
         self.run_in_env = local.get(path.join(path.dirname(__file__), "run_in_env.sh"))
 
@@ -26,10 +26,13 @@ class Command(Generic):
     def _execute(self, component):
         args = []
         try:
-            args += [component["env"], component["command"]]
+            env = component["env"]
+            args += [env, component["command"]]
         except KeyError:
             cmd = local.get(component["command"])
         else:
+            if self._get_env(env) is None:
+                self._create_env(env, ["python<3"], [])
             cmd = self.run_in_env
         try:
             args += component["args"]

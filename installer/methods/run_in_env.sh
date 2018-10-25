a05b7e0a260f2cc51ddb33506b6e287e6a249bb6
Nathan Carlson <nathan.l.carlson@outlook.com>
2018-10-25 13:57:03 -0700

    Improve the reliability of the run_in_env script

--- installer/methods/run_in_env.sh
+++ installer/methods/run_in_env.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
 env_name=$1
+cmd=$2
 source activate $env_name && \
-    eval ${@:2}
+    args="$cmd"
+    # Enquote args to avoid special character problems
+    for arg in ${@:3}; do
+      args="$args '$arg'"
+    done
+    eval "$args"
+    rc=$?
 conda deactivate
+exit $rc

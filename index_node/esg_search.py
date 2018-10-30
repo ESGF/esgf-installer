dac389084afda2b30451c1e9a7ca9940ca2effb7
Nathan Carlson <nathan.l.carlson@outlook.com>
2018-10-30 13:10:47 -0700

    SOLR_HOME is used by the add and remove scripts

--- index_node/esg_search.py
+++ index_node/esg_search.py
@@ -241,12 +241,14 @@ def add_shard(name, port):
 
     add_shard_path = os.path.join(os.path.dirname(__file__), "solr_scripts", "add_shard.sh")
     args = [add_shard_path, name, port]
+    os.environ["SOLR_HOME"] = "/usr/local/solr-home/"
     esg_functions.call_binary("bash", args)
 
 def remove_shard(name, port):
 
     remove_shard_path = os.path.join(os.path.dirname(__file__), "solr_scripts", "remove_shard.sh")
     args = [remove_shard_path, name, port]
+    os.environ["SOLR_HOME"] = "/usr/local/solr-home/"
     esg_functions.call_binary("bash", args)
 
 def main():

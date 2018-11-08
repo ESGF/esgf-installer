87648673786ba725801c719dcffc30371009fb32
Muryanto <muryanto1@ML-9516678.llnl.gov>
2018-11-08 09:27:26 -0800

    add more to the list to run flake8 on

--- tests/test_flake8.py
+++ tests/test_flake8.py
@@ -11,31 +11,37 @@ class test_flake8(unittest.TestCase):
         base_path = os.path.dirname(__file__)
         base_path = os.path.join(base_path, "..")
         base_path = os.path.abspath(base_path)
-        path = os.path.join(base_path, "idp_node")
+        dirs = ["base", "data_node", "filters", "idp_node",
+                "index_node", "shell_scripts"]
 
-        print()
-        print()
-        print()
-        print()
-        print("---------------------------------------------------")
-        print("RUNNING: flake8 on directory %s" % base_path)
-        print("---------------------------------------------------")
-        print()
-        print()
-        print()
-        print()
-        cmd = "flake8 --show-source --statistics " +\
-              "--max-line-length=120 %s" % (path)
-        P = subprocess.Popen(shlex.split(cmd),
-                             stdin=subprocess.PIPE,
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE)
-        out,e = P.communicate()
-        out = out.decode("utf-8")
-        if out != "":
-            print(out)
-        self.assertEqual(out, "")
+        flake8_passed = True
+        for p in base_path, dirs:
+            path = os.path.join(base_path, p)
 
+            print()
+            print()
+            print()
+            print()
+            print("---------------------------------------------------")
+            print("RUNNING: flake8 on directory %s" % path)
+            print("---------------------------------------------------")
+            print()
+            print()
+            print()
+            print()
+            cmd = "flake8 --show-source --statistics " +\
+                "--max-line-length=120 %s" % (path)
+            P = subprocess.Popen(shlex.split(cmd),
+                                 stdin=subprocess.PIPE,
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.PIPE)
+            out,e = P.communicate()
+            out = out.decode("utf-8")
+            if out != "":
+                print(out)
+                flake8_passed = False
+
+        self.assertEqual(flake8_passed, True)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)

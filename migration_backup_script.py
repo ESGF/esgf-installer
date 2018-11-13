3aeb09255236b4486fc67d26582be9db54320f2b
William Hill <hill119@llnl.gov>
2018-11-13 15:10:04 -0800

    added function to copy properties from 2.x config file to 3.0 config file

--- migration_backup_script.py
+++ migration_backup_script.py
@@ -9,8 +9,24 @@ from base import esg_postgres
 def check_previous_install():
     return os.path.exists("/usr/local/bin/esg-node")
 
+def copy_previous_settings(old_config_file, new_config_file):
+    print "\n*******************************"
+    print "Copying settings from 2.x esgf.properties to 3.0 esgf.properties file"
+    print "******************************* \n"
+    old_parser = configparser.ConfigParser()
+    old_parser.read(old_config_file)
 
-#TODO: Create a function to port values from old config files to new esgf.properties file
+    previous_values = dict(old_parser.items('installer.properties'))
+
+    new_parser = configparser.ConfigParser()
+    new_parser.read(new_config_file)
+
+    for key, value in previous_values.iteritems():
+        new_parser["installer.properties"][key] = str(value)
+
+    with open(new_config_file, "w") as file_object:
+        new_parser.write(file_object, space_around_delimiters=False)
+
 
 def add_config_file_section_header(config_file_name, section_header):
     '''INI-style config files in 2.x typically do not have section headers that are required to be parsed by ConfigParser. This function will add the required section headers'''
@@ -21,9 +37,6 @@ def add_config_file_section_header(config_file_name, section_header):
     config.write('[{}]\n'.format(section_header))
     config.write(open(config_file_name).read())
     config.seek(0, os.SEEK_SET)
-
-    # parser = configparser.ConfigParser()
-    # parser.read(config)
 
     with open(config_file_name, "w") as file_object:
         shutil.copyfileobj(config, file_object)
@@ -54,12 +67,17 @@ def backup_esg_installation():
     for file_name in files_to_backup:
         esg_functions.create_backup_file(file_name, backup_dir=migration_backup_dir)
 
-    add_config_file_section_header(os.path.join(migration_backup_dir, "esgf.properties-{}.bak".format(str(datetime.date.today()))), "installer.properties")
-    add_config_file_section_header(os.path.join(migration_backup_dir, "esgf-install-manifest-{}.bak".format(str(datetime.date.today()))), "install_manifest")
+    properties_backup_path = os.path.join(migration_backup_dir, "esgf.properties-{}.bak".format(str(datetime.date.today())))
+    add_config_file_section_header(properties_backup_path, "installer.properties")
+    install_manifest_backup_path = os.path.join(migration_backup_dir, "esgf-install-manifest-{}.bak".format(str(datetime.date.today())))
+    add_config_file_section_header(install_manifest_backup_path, "install_manifest")
 
     directories_to_backup = ["/usr/local/tomcat", "/usr/local/solr", "/etc/grid-security", "/esg/config", "/usr/local/cog/cog_config", "/etc/esgfcerts", "/etc/certs"]
     for directory in directories_to_backup:
         esg_functions.backup(directory, migration_backup_dir)
+
+    current_directory = os.path.join(os.path.dirname(__file__))
+    copy_previous_settings(properties_backup_path, os.path.join(current_directory, "esgf.properties.template"))
 
 
 
